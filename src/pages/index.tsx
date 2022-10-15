1e98c29893d1ecc0f5071c49cc064ca99cf3f968
Võ Hoàng Phúc <phucvo0708@gmail.com>
2022-10-15 16:35:31 +0700

    Added tailwind CSS and basic index setup

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,33 @@
 import type { NextPage } from "next";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 const Home: NextPage = () => {
-  return <div>Tuturuuu App</div>;
+  const router = useRouter();
+  const devMode = process.env.NODE_ENV === "development";
+
+  const authProdUrl = "https://auth.tuturuuu.com";
+  const authDevUrl = "http://localhost:7802";
+
+  useEffect(() => {
+    const cookie = document.cookie
+      .split(";")
+      .find((c) => c.includes("tuturuuu-auth"));
+
+    if (!cookie) {
+      router.push(
+        (devMode ? authDevUrl : authProdUrl) +
+          "?nextUrl=" +
+          window.location.href
+      );
+    }
+  }, [devMode, authDevUrl, authProdUrl, router]);
+
+  return (
+    <div className="h-screen w-screen bg-zinc-900 text-white flex items-center justify-center text-6xl font-semibold">
+      Application
+    </div>
+  );
 };
 
 export default Home;
