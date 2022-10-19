768a4ce75c47fd124e6c64d0ed66d013c9a1da19
Võ Hoàng Phúc <phucvo0708@gmail.com>
2022-10-19 11:45:52 +0700

    Added dynamic app version getter

---
+++ src/core/constants.ts
@@ -0,0 +1,7 @@
+import { version } from './version';
+
+export const BASE_URL = process.env.BASE_URL || 'http://localhost:7803';
+export const AUTH_URL = process.env.AUTH_URL || 'http://localhost:7802';
+export const API_URL = process.env.API_URL || 'http://localhost:7801';
+
+export const APP_VERSION = version;
