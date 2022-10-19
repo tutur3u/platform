b8820fb271120700b62b1b055bf3240a5fbc3d11
Thu Nguyen <101914949+npat273@users.noreply.github.com>
2022-10-19 23:02:50 +0700

    Added mantine, heroicons, babel

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,24 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import { MantineProvider } from '@mantine/core';
+import { AppWithLayoutProps } from '../types/AppWithLayoutProps';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function Application({
+  Component,
+  pageProps,
+}: AppWithLayoutProps) {
+  // Use the layout defined at the page level, if available
+  const getLayout = Component.getLayout ?? ((page) => page);
+
+  return (
+    <MantineProvider
+      withGlobalStyles
+      withNormalizeCSS
+      theme={{
+        /** Put your mantine theme override here */
+        colorScheme: 'dark',
+      }}
+    >
+      {getLayout(<Component {...pageProps} />)}
+    </MantineProvider>
+  );
 }
-
-export default MyApp
