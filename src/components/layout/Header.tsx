5f739563245b6c75998a77fa62ee83a0419f50f7
Võ Hoàng Phúc <phucvo0708@gmail.com>
2022-10-23 22:31:34 +0700

    Added development build tag for env environment

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,23 +1,23 @@
 import { BellIcon, MagnifyingGlassIcon } from '@heroicons/react/24/outline';
-import { useUser } from '../../hooks/useUser';
+import { DEV_MODE } from '../../constants/common';
 
 interface HeaderProps {
   className?: string;
 }
 
 export default function Header({ className }: HeaderProps) {
-  const { user } = useUser();
-
   return (
     <div className={`${className} w-full flex justify-between items-center`}>
-      <div className="text-xl font-semibold">
-        <span>{user ? user?.email || user?.phone : 'Not logged in'}</span>
-      </div>
-      <div className="flex justify-around items-center">
-        <button className="p-2 hover:bg-zinc-300/10 hover:text-zinc-300 hover:border-zinc-600 rounded-full hover:cursor-pointer transition duration-150">
+      {DEV_MODE && (
+        <div className="px-4 py-2 text-xl font-semibold bg-purple-300/20 text-purple-300 rounded cursor-default">
+          Development build
+        </div>
+      )}
+      <div className="flex items-center gap-2">
+        <button className="p-2 hover:bg-zinc-300/10 hover:text-zinc-300 hover:border-zinc-600 rounded hover:cursor-pointer transition duration-150">
           <MagnifyingGlassIcon className="w-6 h-6" />
         </button>
-        <button className="p-2 hover:bg-zinc-300/10 hover:text-zinc-300 hover:border-zinc-600 rounded-full hover:cursor-pointer transition duration-150">
+        <button className="p-2 hover:bg-zinc-300/10 hover:text-zinc-300 hover:border-zinc-600 rounded hover:cursor-pointer transition duration-150">
           <BellIcon className="w-6 h-6" />
         </button>
       </div>
