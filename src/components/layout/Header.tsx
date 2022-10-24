31809ae3f4b62a2ad5a1e987f90e9bfcabf36240
Võ Hoàng Phúc <phucvo0708@gmail.com>
2022-10-24 15:51:12 +0700

    Fix header alignment bug

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -8,10 +8,12 @@ interface HeaderProps {
 export default function Header({ className }: HeaderProps) {
   return (
     <div className={`${className} w-full flex justify-between items-center`}>
-      {DEV_MODE && (
+      {DEV_MODE ? (
         <div className="px-4 py-2 text-xl font-semibold bg-purple-300/20 text-purple-300 rounded cursor-default">
           Development build
         </div>
+      ) : (
+        <div />
       )}
       <div className="flex items-center gap-2">
         <button className="p-2 hover:bg-zinc-300/10 hover:text-zinc-300 hover:border-zinc-600 rounded hover:cursor-pointer transition duration-150">
