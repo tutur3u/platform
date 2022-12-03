63d5dbe48524c31d4b6d1e84168d5f9f2ad87630
Thu Nguyen <101914949+npat273@users.noreply.github.com>
2022-12-03 21:26:15 +0700

    Auto generate day, month, year

--- src/components/calendar/DayTittle.tsx
+++ src/components/calendar/DayTittle.tsx
@@ -1,17 +1,25 @@
 interface DayTitleProps {
-  date: string;
+  date: Date;
   weekday: string;
 }
 
 export default function DayTitle({ date, weekday }: DayTitleProps) {
+  const today = new Date();
+  const isToday = date.toDateString() === today.toDateString();
   return (
-    <div>
-      <div className="flex h-20 flex-col items-center justify-center text-xl font-semibold text-purple-500/70">
-        {weekday}
-        <span className="mt-1 flex h-10 w-10 items-center justify-center rounded-full bg-purple-500/70 text-2xl text-white">
-          {date}
-        </span>
-      </div>
+    <div
+      className={`flex h-20 flex-col items-center justify-center text-xl font-semibold ${
+        isToday ? 'text-purple-500/70' : ''
+      }`}
+    >
+      {weekday}
+      <span
+        className={`${
+          isToday ? 'bg-purple-500/70' : ''
+        } mt-1 flex h-10 w-10 items-center justify-center rounded-full text-2xl text-white`}
+      >
+        {date.getDate()}
+      </span>
     </div>
   );
 }
--- src/pages/calendar/index.tsx
+++ src/pages/calendar/index.tsx
@@ -1,5 +1,5 @@
 import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/24/solid';
-import { ReactElement, useEffect } from 'react';
+import { ReactElement, useEffect, useState } from 'react';
 import DayTittle from '../../components/calendar/DayTittle';
 import Layout from '../../components/layout/Layout';
 import { useAppearance } from '../../hooks/useAppearance';
@@ -9,6 +9,8 @@ import { PageWithLayoutProps } from '../../types/PageWithLayoutProps';
 const CalendarPage: PageWithLayoutProps = () => {
   const { setRootSegment } = useAppearance();
   const { clearUsers } = useUserList();
+
+  const [date, setDate] = useState(new Date());
 
   useEffect(() => {
     setRootSegment({
@@ -21,25 +23,77 @@ const CalendarPage: PageWithLayoutProps = () => {
   }, []);
 
   const weekdays = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
-  const dates = ['1', '2', '3', '4', '5', '6', '7'];
+
+  const setToday = () => {
+    setDate(new Date());
+  };
+
+  const setPreviousWeek = () => {
+    const newDate = new Date(date);
+    newDate.setDate(newDate.getDate() - 7);
+    setDate(newDate);
+  };
+
+  const setNextWeek = () => {
+    const newDate = new Date(date);
+    newDate.setDate(newDate.getDate() + 7);
+    setDate(newDate);
+  };
+
+  const getMonday = () => {
+    const day = date.getDay() || 7;
+
+    if (day !== 1) {
+      date.setHours(-24 * (day - 1));
+    }
+    return date;
+  };
+
+  // get other date from monday to sunday
+  const getWeekdays = () => {
+    const monday = getMonday();
+    const days = [];
+
+    for (let i = 0; i < 7; i++) {
+      const day = new Date(monday);
+      day.setDate(day.getDate() + i);
+      days.push(day);
+    }
+    return days;
+  };
+
+  const shortMonthName = new Intl.DateTimeFormat('en-US', { month: 'long' })
+    .format;
+  const longMonth = shortMonthName(date); // "Jul"
 
   return (
     <div className="flex h-full min-h-full w-full flex-col rounded-lg bg-purple-300/10 p-5">
       <div className="mb-8 flex h-10 justify-between">
-        <div className="text-3xl font-semibold">December 2022</div>
+        <div className="text-3xl font-semibold">
+          {longMonth} <span>{date.getFullYear()}</span>
+        </div>
         <div className="flex items-center justify-center gap-3">
-          <ChevronLeftIcon className="w-9 rounded-xl p-2 text-3xl hover:cursor-pointer hover:bg-zinc-500"></ChevronLeftIcon>
-          <div className="cursor-pointer rounded-xl bg-zinc-500 p-2 text-lg font-semibold">
+          <ChevronLeftIcon
+            onClick={setPreviousWeek}
+            className="w-9 rounded-xl p-2 text-3xl hover:cursor-pointer hover:bg-zinc-500"
+          ></ChevronLeftIcon>
+          <button
+            onClick={setToday}
+            className="cursor-pointer rounded-xl bg-purple-300/20 p-2 text-lg font-semibold"
+          >
             Today
-          </div>
-          <ChevronRightIcon className="w-9 rounded-xl p-2 text-3xl hover:cursor-pointer hover:bg-zinc-500"></ChevronRightIcon>
+          </button>
+          <ChevronRightIcon
+            onClick={setNextWeek}
+            className="w-9 rounded-xl p-2 text-3xl hover:cursor-pointer hover:bg-zinc-500"
+          ></ChevronRightIcon>
         </div>
       </div>
       <div className="grid grid-cols-8">
         <div className="col-span-1"></div>
         {weekdays.map((weekday, index) => (
           <div key={index} className="col-span-1">
-            <DayTittle date={dates[index]} weekday={weekday} />
+            <DayTittle date={getWeekdays()[index]} weekday={weekday} />
           </div>
         ))}
         <div className="col-span-1 flex h-10 items-center justify-center">
