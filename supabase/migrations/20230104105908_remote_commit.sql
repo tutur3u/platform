5e020306f0bc3b7569124aea86f421045d52712b
Võ Hoàng Phúc <contact@vohoangphuc.com>
2023-01-16 20:53:34 +0700

    Updated supabase migration queries

--- supabase/migrations/20230104105908_remote_commit.sql
+++
@@ -1,6 +0,0 @@
-CREATE TRIGGER create_profile_for_new_user_tr BEFORE
-INSERT ON auth.users FOR EACH ROW EXECUTE FUNCTION create_profile_for_new_user();
-create policy "Enable insert for authenticated users only" on "storage"."buckets" as permissive for
-insert to authenticated with check (true);
-create policy "Enable insert for authenticated users only" on "storage"."objects" as permissive for
-insert to authenticated with check (true);
