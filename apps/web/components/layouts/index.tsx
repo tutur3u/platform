eab7dc3f79b73de20d3c64119939df3a88438ed6
Võ Hoàng Phúc <contact@vohoangphuc.com>
2023-02-02 21:50:28 +0700

    feat: add 404 and 500 error pages for apps/web

--- apps/web/components/layouts/index.tsx
+++ apps/web/components/layouts/index.tsx
@@ -3,15 +3,16 @@ import Navbar from "./Navbar";
 import Footer from "./Footer";
 
 interface LayoutProps {
+  hideSlogan?: boolean;
   children: React.ReactNode;
 }
 
-const Layout: FC<LayoutProps> = ({ children }: LayoutProps) => {
+const Layout: FC<LayoutProps> = ({ hideSlogan, children }: LayoutProps) => {
   return (
     <div className="relative">
       <Navbar />
-      <div>{children}</div>
-      <Footer />
+      <div className="relative min-h-screen">{children}</div>
+      <Footer hideSlogan={hideSlogan} />
     </div>
   );
 };
---
+++ apps/web/pages/404.tsx
@@ -0,0 +1,32 @@
+import React, { ReactElement } from "react";
+import { PageWithLayoutProps } from "../types/PageWithLayoutProps";
+import Layout from "../components/layouts";
+import Link from "next/link";
+
+const Error404Page: PageWithLayoutProps = () => {
+  return (
+    <div className="absolute inset-0 flex flex-col items-center justify-center mt-24 mb-8 mx-4 md:mx-32 lg:mx-64 text-center">
+      <h1 className="text-9xl font-bold">
+        <span className="text-orange-300">4</span>
+        <span className="text-green-300">0</span>
+        <span className="text-red-300">4</span>
+      </h1>
+      <p className="text-xl text-zinc-300 font-semibold">
+        The page you are looking for does not exist.
+      </p>
+
+      <Link
+        href="/"
+        className="block w-fit mt-4 bg-blue-300/20 hover:bg-blue-300/30 text-blue-300 hover:text-blue-200 rounded-lg px-8 py-2 font-semibold transition duration-300"
+      >
+        Go back to home
+      </Link>
+    </div>
+  );
+};
+
+Error404Page.getLayout = function getLayout(page: ReactElement) {
+  return <Layout hideSlogan>{page}</Layout>;
+};
+
+export default Error404Page;
