526b463110ee3931a0b89f43d4b61a1295982279
Võ Hoàng Phúc <contact@vohoangphuc.com>
2023-02-04 17:05:32 +0700

    chore: disable under-construction notice in dev mode

--- apps/app/src/components/calendar/Calendar.tsx
+++ apps/app/src/components/calendar/Calendar.tsx
@@ -5,7 +5,7 @@ import WeekdayBar from './WeekdayBar';
 
 const Calendar = () => {
   return (
-    <div className="flex h-full w-full flex-col border-zinc-800 bg-zinc-900 p-6">
+    <div className="flex h-full w-full flex-col p-6">
       <CalendarHeader />
       <WeekdayBar />
       <CalendarViewWithTrail />
--- apps/app/src/pages/tasks/index.tsx
+++ apps/app/src/pages/tasks/index.tsx
@@ -5,6 +5,7 @@ import { useAppearance } from '../../hooks/useAppearance';
 import { useUserData } from '../../hooks/useUserData';
 import { useUserList } from '../../hooks/useUserList';
 import { PageWithLayoutProps } from '../../types/PageWithLayoutProps';
+import { DEV_MODE } from '../../constants/common';
 
 const TasksPage: PageWithLayoutProps = () => {
   const { setRootSegment, changeLeftSidebarSecondaryPref } = useAppearance();
@@ -25,9 +26,19 @@ const TasksPage: PageWithLayoutProps = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [data]);
 
+  if (!DEV_MODE)
+    return (
+      <div className="h-full p-4 md:p-8">
+        <div className="flex h-full min-h-full w-full items-center justify-center rounded-lg border border-purple-300/20 bg-purple-300/10 text-6xl font-semibold text-purple-300">
+          Under construction 🚧
+        </div>
+      </div>
+    );
+
   return (
     <>
       <HeaderX label="Tasks" />
+      <div className="flex-colp-6 flex h-full w-full"></div>
     </>
   );
 };
