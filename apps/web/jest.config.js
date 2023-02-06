6488cfd0699bcbc8d59518daba621a518cedf284
Võ Hoàng Phúc <contact@vohoangphuc.com>
2023-02-06 12:45:29 +0700

    bug: fix invalid jest config

--- apps/web/jest.config.js
+++ apps/web/jest.config.js
@@ -1,4 +1,4 @@
-import nextJest from 'next/jest';
+const nextJest = require('next/jest');
 
 const createJestConfig = nextJest({
   // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
@@ -17,4 +17,4 @@ const customJestConfig = {
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
-export default createJestConfig(customJestConfig);
+module.exports = createJestConfig(customJestConfig);
