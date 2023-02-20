33cb99787f096cd2c747bca461aa58a613e7a3e2
Võ Hoàng Phúc <contact@vohoangphuc.com>
2023-02-20 16:18:06 +0700

    db: fix task_board_member dependency

---
+++ apps/web/supabase/migrations/20230220085348_remote_commit.sql
@@ -0,0 +1,125 @@
+set check_function_bodies = off;
+
+CREATE OR REPLACE FUNCTION auth.email()
+ RETURNS text
+ LANGUAGE sql
+ STABLE
+AS $function$
+  select 
+  	coalesce(
+		nullif(current_setting('request.jwt.claim.email', true), ''),
+		(nullif(current_setting('request.jwt.claims', true), '')::jsonb ->> 'email')
+	)::text
+$function$
+;
+
+CREATE OR REPLACE FUNCTION auth.role()
+ RETURNS text
+ LANGUAGE sql
+ STABLE
+AS $function$
+  select 
+  	coalesce(
+		nullif(current_setting('request.jwt.claim.role', true), ''),
+		(nullif(current_setting('request.jwt.claims', true), '')::jsonb ->> 'role')
+	)::text
+$function$
+;
+
+CREATE OR REPLACE FUNCTION auth.uid()
+ RETURNS uuid
+ LANGUAGE sql
+ STABLE
+AS $function$
+  select 
+  	coalesce(
+		nullif(current_setting('request.jwt.claim.sub', true), ''),
+		(nullif(current_setting('request.jwt.claims', true), '')::jsonb ->> 'sub')
+	)::uuid
+$function$
+;
+
+
+drop policy "Enable read access for members of the task board" on "public"."task_board_members";
+
+alter table "public"."task_board_members" drop constraint "task_board_members_user_id_fkey";
+
+alter table "public"."task_board_members" drop constraint "task_board_members_pkey";
+
+drop index if exists "public"."task_board_members_pkey";
+
+drop table "public"."task_board_members";
+
+set check_function_bodies = off;
+
+CREATE OR REPLACE FUNCTION public.is_org_member(_user_id uuid, _org_id uuid)
+ RETURNS boolean
+ LANGUAGE sql
+ SECURITY DEFINER
+AS $function$SELECT EXISTS (
+  SELECT 1
+  FROM org_members om
+  WHERE om.org_id = _org_id
+  AND om.user_id = _user_id
+);$function$
+;
+
+CREATE OR REPLACE FUNCTION public.is_project_member(_project_id uuid)
+ RETURNS boolean
+ LANGUAGE sql
+ SECURITY DEFINER
+AS $function$
+SELECT EXISTS (
+  SELECT 1
+  FROM projects
+  WHERE id = _project_id
+);
+$function$
+;
+
+
+set check_function_bodies = off;
+
+CREATE OR REPLACE FUNCTION storage.extension(name text)
+ RETURNS text
+ LANGUAGE plpgsql
+AS $function$
+DECLARE
+_parts text[];
+_filename text;
+BEGIN
+	select string_to_array(name, '/') into _parts;
+	select _parts[array_length(_parts,1)] into _filename;
+	-- @todo return the last part instead of 2
+	return split_part(_filename, '.', 2);
+END
+$function$
+;
+
+CREATE OR REPLACE FUNCTION storage.filename(name text)
+ RETURNS text
+ LANGUAGE plpgsql
+AS $function$
+DECLARE
+_parts text[];
+BEGIN
+	select string_to_array(name, '/') into _parts;
+	return _parts[array_length(_parts,1)];
+END
+$function$
+;
+
+CREATE OR REPLACE FUNCTION storage.foldername(name text)
+ RETURNS text[]
+ LANGUAGE plpgsql
+AS $function$
+DECLARE
+_parts text[];
+BEGIN
+	select string_to_array(name, '/') into _parts;
+	return _parts[1:array_length(_parts,1)-1];
+END
+$function$
+;
+
+
