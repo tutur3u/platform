ca5e1f83c3446eb627fd0927c7ffdcbe17f03b5d
Võ Hoàng Phúc <contact@vohoangphuc.com>
2023-02-24 13:01:36 +0700

    fix: resolve supabase migrations table conflicts

--- apps/web/src/pages/finance/index.tsx
+++ apps/web/src/pages/finance/index.tsx
@@ -117,18 +117,6 @@ const FinancePage: PageWithLayoutProps = () => {
     });
   };
 
-  if (!DEV_MODE)
-    return (
-      <>
-        <HeaderX label="Finance" />
-        <div className="p-4 md:h-screen md:p-8">
-          <div className="flex h-full min-h-full w-full items-center justify-center rounded-lg border border-purple-300/20 bg-purple-300/10 p-8 text-center text-2xl font-semibold text-purple-300 md:text-6xl">
-            Under construction 🚧
-          </div>
-        </div>
-      </>
-    );
-
   return (
     <>
       <div className="flex w-full">
