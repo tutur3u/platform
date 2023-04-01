f0158a52803affe94a069800d14d60b23a148fc9
Võ Hoàng Phúc <contact@vohoangphuc.com>
2023-04-01 14:02:38 +0700

    fix: add default font sizes to Mantine theme

--- apps/web/src/styles/mantine-theme.ts
+++ apps/web/src/styles/mantine-theme.ts
@@ -2,6 +2,14 @@ import { MantineThemeOverride } from '@mantine/core';
 
 export const theme: MantineThemeOverride = {
   colorScheme: 'dark',
+  fontSizes: {
+    xs: '1rem',
+    sm: '1rem',
+    md: '1rem',
+    lg: '1rem',
+    xl: '1rem',
+    xxl: '1rem',
+  },
   focusRingStyles: {
     resetStyles: () => ({ outline: 'none' }),
     styles: () => ({ outline: 'none' }),
