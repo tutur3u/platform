d8333983806b5aa96f38cf9782b33dd8609841af
Võ Hoàng Phúc <contact@vohoangphuc.com>
2023-04-02 21:00:36 +0700

    feat: add i18n support for underconstruction pages

--- apps/web/src/pages/[wsId]/activities/index.tsx
+++ apps/web/src/pages/[wsId]/activities/index.tsx
@@ -5,6 +5,7 @@ import { enforceHasWorkspaces } from '../../../utils/serverless/enforce-has-work
 import { useSegments } from '../../../hooks/useSegments';
 import { useWorkspaces } from '../../../hooks/useWorkspaces';
 import SidebarLayout from '../../../components/layouts/SidebarLayout';
+import UnderConstructionTag from '../../../components/common/UnderConstructionTag';
 
 export const getServerSideProps = enforceHasWorkspaces;
 
@@ -32,11 +33,7 @@ const HistoryPage: PageWithLayoutProps = () => {
   return (
     <>
       <HeaderX label="Classes" />
-      <div className="p-4 md:h-screen md:p-8">
-        <div className="flex h-full min-h-full w-full items-center justify-center rounded-lg border border-purple-300/20 bg-purple-300/10 p-8 text-center text-2xl font-semibold text-purple-300 md:text-6xl">
-          Under construction 🚧
-        </div>
-      </div>
+      <UnderConstructionTag />
     </>
   );
 
