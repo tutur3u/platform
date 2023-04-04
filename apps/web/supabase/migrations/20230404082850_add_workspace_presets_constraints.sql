ad57eb878d0a400ba078476687418730bc56a9e0
Võ Hoàng Phúc <contact@vohoangphuc.com>
2023-04-04 16:19:39 +0700

    db: fix migration file

--- apps/web/supabase/migrations/20230404082850_add_workspace_presets_constraints.sql
+++ apps/web/supabase/migrations/20230404082850_add_workspace_presets_constraints.sql
@@ -1,8 +1,8 @@
--- Make sure public.workspaces.preset references public.workspace_presets.name and it's not null
--- The default value for it is "GENERAL"
 ALTER TABLE public.workspaces
 ALTER COLUMN preset
 SET DEFAULT 'GENERAL';
+-- Drop the workspaces_preset_fkey if it exists
+ALTER TABLE public.workspaces DROP CONSTRAINT IF EXISTS workspaces_preset_fkey;
 ALTER TABLE public.workspaces
 ADD CONSTRAINT workspaces_preset_fkey FOREIGN KEY (preset) REFERENCES public.workspace_presets(name);
 ALTER TABLE public.workspaces
