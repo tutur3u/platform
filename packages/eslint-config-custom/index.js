64472ce7e57e56421932c3c76e2c30774f94b77b
Võ Hoàng Phúc <contact@vohoangphuc.com>
2023-04-22 18:47:40 +0700

    chore: format files using prettier

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,8 +1,9 @@
 module.exports = {
-  extends: ['next', 'turbo', 'prettier'],
+  extends: ['next/babel', 'next/core-web-vitals', 'turbo', 'prettier'],
   rules: {
     '@next/next/no-html-link-for-pages': 'off',
   },
+  ignorePatterns: ['node_modules', 'dist'],
   parserOptions: {
     babelOptions: {
       presets: [require.resolve('next/babel')],
