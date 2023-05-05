14baaa98301add59ba12634c45df6ac479abe43e
Võ Hoàng Phúc <contact@vohoangphuc.com>
2023-05-05 11:55:30 +0700

    chore: remove custom webpack config

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -5,17 +5,6 @@ const nextConfig = nextTranslate({
   reactStrictMode: true,
   transpilePackages: ['ui'],
 
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      // don't resolve 'fs' module on the client to prevent this error on build --> Error: Can't resolve 'fs'
-      config.resolve.fallback = {
-        fs: false,
-      };
-    }
-
-    return config;
-  },
-
   rewrites() {
     return [
       {
