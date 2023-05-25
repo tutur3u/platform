3523c6f8dd8061815439afe80d5b80e1bef69627
Thu Nguyen <101914949+npat273@users.noreply.github.com>
2023-05-25 22:24:55 +0700

    style: modify LogoTab

--- apps/web/src/components/branding/LogoTab.tsx
+++ apps/web/src/components/branding/LogoTab.tsx
@@ -20,15 +20,15 @@ export default function LogoTab({
     : "bg-[url('/media/background/transparent-light.png')]";
   return (
     <>
-      <div className={`${bgCss} relative flex items-center justify-center p-4`}>
+      <div className={`${bgCss} rounded-xl relative flex items-center justify-center p-2`}>
         <Image
           width={25}
           height={25}
-          className="h-full w-full"
+          className="h-48 w-48"
           src={logoImage}
           alt={alt}
         />
-        <div className="absolute inset-0 flex items-center justify-center gap-5 opacity-0 hover:opacity-100">
+        <div className="absolute inset-0 m-2 flex items-end justify-start gap-2 opacity-0 hover:opacity-100">
           <button className="rounded bg-black/90 px-4 py-2 text-sm font-bold text-white/50 transition duration-300 hover:text-white">
             <a href={svgLink} download>
               .svg
