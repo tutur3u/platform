85fa73c1657df67f1ab5a137fed2120db1a3035b
Võ Hoàng Phúc <contact@vohoangphuc.com>
2023-08-26 13:55:25 +0700

    feat: migrate 404 and 500 error pages to app dir

--- apps/web/src/app/error.tsx
+++ apps/web/src/app/error.tsx
@@ -1,5 +1,6 @@
 'use client'; // Error components must be Client Components
 
+import { Button } from '@/components/ui/button';
 import { useEffect } from 'react';
 
 export default function Error({
@@ -15,16 +16,13 @@ export default function Error({
   }, [error]);
 
   return (
-    <div className="flex h-screen w-screen flex-col items-center justify-center gap-2">
-      <h2 className="font-semibold">Something went wrong!</h2>
-      <button
-        onClick={
-          // Attempt to recover by trying to re-render the segment
-          () => reset()
-        }
-      >
-        Try again
-      </button>
+    <div className="absolute inset-0 mx-4 mb-8 mt-24 flex flex-col items-center justify-center text-center md:mx-32 lg:mx-64">
+      <h1 className="text-xl font-bold">Something went wrong.</h1>
+      <p className="mb-4 font-semibold opacity-75">
+        {error?.message || error?.digest || 'Unknown error'}
+      </p>
+
+      <Button onClick={() => reset()}>Try again</Button>
     </div>
   );
 }
