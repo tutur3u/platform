60c9a8657ef3ae12bc0c71cad728db781de10af0
Võ Hoàng Phúc <contact@vohoangphuc.com>
2023-08-31 22:57:11 +0700

    feat: add root workspace check utility functions

--- apps/web/src/lib/workspace-helper.ts
+++ apps/web/src/lib/workspace-helper.ts
@@ -2,8 +2,9 @@ import { notFound, redirect } from 'next/navigation';
 import { cookies } from 'next/headers';
 import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
 import { Workspace } from '@/types/primitives/Workspace';
+import { ROOT_WORKSPACE_ID } from '@/constants/common';
 
-export async function getWorkspace(id?: string | null) {
+export async function getWorkspace(id?: string) {
   if (!id) notFound();
 
   const supabase = createServerComponentClient({ cookies });
@@ -49,4 +50,29 @@ export async function getWorkspaces() {
   if (error) notFound();
 
   return data as Workspace[];
+}
+export function enforceRootWorkspace(wsId: string) {
+  if (wsId !== ROOT_WORKSPACE_ID) notFound();
+}
+
+export async function enforceRootWorkspaceAdmin(wsId: string) {
+  enforceRootWorkspace(wsId);
+
+  const supabase = createServerComponentClient({ cookies });
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (!user) redirect('/login');
+
+  const { error } = await supabase
+    .from('workspace_members')
+    .select('role')
+    .eq('ws_id', ROOT_WORKSPACE_ID)
+    .eq('user_id', user.id)
+    .eq('role', 'OWNER')
+    .single();
+
+  if (error) notFound();
 }
