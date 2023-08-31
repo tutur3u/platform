99bb62b419d6554e3c1541dcb5e3f1b22fb06e86
Võ Hoàng Phúc <contact@vohoangphuc.com>
2023-08-31 11:49:49 +0700

    feat(documents): convert overview page to RSC (WIP)

--- apps/web/src/app/(dashboard)/[wsId]/documents/page.tsx
+++ apps/web/src/app/(dashboard)/[wsId]/documents/page.tsx
@@ -1,183 +1,111 @@
-'use client';
-
-import { useEffect, useState } from 'react';
-import { useSegments } from '../../../../hooks/useSegments';
 import DocumentCard from '../../../../components/document/DocumentCard';
-import { useUser } from '@supabase/auth-helpers-react';
-import useSWR from 'swr';
 import { Document } from '../../../../types/primitives/Document';
-import { Divider, Loader } from '@mantine/core';
-import { openModal } from '@mantine/modals';
-import DocumentEditForm from '../../../../components/forms/DocumentEditForm';
-import { showNotification } from '@mantine/notifications';
-import { useRouter } from 'next/navigation';
-import {
-  DocumentPlusIcon,
-  ListBulletIcon,
-  Squares2X2Icon,
-} from '@heroicons/react/24/solid';
-import { useWorkspaces } from '../../../../hooks/useWorkspaces';
+import { DocumentPlusIcon } from '@heroicons/react/24/solid';
 import useTranslation from 'next-translate/useTranslation';
+import { Separator } from '@/components/ui/separator';
+import { getWorkspace } from '@/lib/workspace-helper';
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
+import { cookies } from 'next/headers';
 
-export default function DocumentsPage() {
-  const router = useRouter();
-  const user = useUser();
+interface Props {
+  params: {
+    wsId: string;
+  };
+}
 
-  const { ws } = useWorkspaces();
-  const { setRootSegment } = useSegments();
+export const dynamic = 'force-dynamic';
 
-  const { data: workspace } = useSWR(
-    ws?.id ? `/api/workspaces/${ws?.id}` : null
-  );
+export default async function DocumentsPage({ params: { wsId } }: Props) {
+  const ws = await getWorkspace(wsId);
+  const documents = await getDocuments(wsId);
 
   const { t } = useTranslation('documents');
 
-  const documentsLabel = t('documents');
-
-  const creatingLabel = t('creating');
   const newDocumentLabel = t('new-document');
   const noDocumentsLabel = t('no-documents');
-  const createDocumentErrorLabel = t('create-document-error');
-  const createNewDocumentLabel = t('create-new-document');
+  // const createDocumentErrorLabel = t('create-document-error');
+  // const createNewDocumentLabel = t('create-new-document');
 
-  useEffect(() => {
-    setRootSegment(
-      ws?.id
-        ? [
-            {
-              content: workspace?.name || 'Unnamed Workspace',
-              href: `/${ws.id}`,
-            },
-            { content: documentsLabel, href: `/${ws.id}/documents` },
-          ]
-        : []
-    );
-  }, [ws?.id, workspace?.name, documentsLabel, setRootSegment]);
+  // async function createDocument({
+  //   wsId,
+  //   doc,
+  // }: {
+  //   wsId: string;
+  //   doc: Partial<Document>;
+  // }) {
+  //   if (!ws) return;
 
-  const { data: documents } = useSWR<Document[]>(
-    user?.id && ws?.id ? `/api/workspaces/${ws.id}/documents` : null
-  );
+  //   const res = await fetch(`/api/workspaces/${ws.id}/documents`, {
+  //     method: 'POST',
+  //     headers: {
+  //       'Content-Type': 'application/json',
+  //     },
+  //     body: JSON.stringify({
+  //       name: doc.name,
+  //     }),
+  //   });
 
-  const [creating, setCreating] = useState(false);
-  const [mode, setMode] = useState<'list' | 'grid'>('grid');
+  //   if (!res.ok) {
+  // showNotification({
+  //   title: 'Error',
+  //   message: createDocumentErrorLabel,
+  //   color: 'red',
+  // });
+  //     return;
+  //   }
 
-  const createDocument = async ({
-    wsId,
-    doc,
-  }: {
-    wsId: string;
-    doc: Partial<Document>;
-  }) => {
-    if (!ws) return;
-    setCreating(true);
+  //   const { id } = await res.json();
+  //   redirect(`/${wsId}/documents/${id}`);
+  // }
 
-    const res = await fetch(`/api/workspaces/${ws.id}/documents`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        name: doc.name,
-      }),
-    });
-
-    if (!res.ok) {
-      showNotification({
-        title: 'Error',
-        message: createDocumentErrorLabel,
-        color: 'red',
-      });
-      return;
-    }
-
-    const { id } = await res.json();
-    router.push(`/${wsId}/documents/${id}`);
-  };
-
-  const showDocumentEditForm = () => {
-    if (!ws) return;
-    openModal({
-      title: <div className="font-semibold">{createNewDocumentLabel}</div>,
-      centered: true,
-      children: (
-        <DocumentEditForm
-          onSubmit={(doc) => createDocument({ wsId: ws?.id, doc })}
-        />
-      ),
-    });
-  };
+  // function showDocumentEditForm() {
+  // openModal({
+  //   title: <div className="font-semibold">{createNewDocumentLabel}</div>,
+  //   centered: true,
+  //   children: (
+  //     <DocumentEditForm
+  //       onSubmit={(doc) => createDocument({ wsId: ws?.id, doc })}
+  //     />
+  //   ),
+  // });
+  // }
 
   return (
     <>
       <div className="flex flex-col items-center gap-4 md:flex-row">
-        <h1 className="w-full flex-grow rounded-lg bg-zinc-500/5 p-4 text-2xl font-bold dark:bg-zinc-900 md:w-auto">
-          {documentsLabel}{' '}
-          <span className="rounded-lg bg-purple-500/10 px-2 text-lg text-purple-600 dark:bg-purple-300/20 dark:text-purple-300">
-            {documents?.length || 0}
-          </span>
-        </h1>
-
-        <div className="flex gap-4">
-          <button
-            onClick={showDocumentEditForm}
-            className="flex flex-none items-center gap-1 rounded bg-blue-500/10 p-4 font-semibold text-blue-600 transition hover:bg-blue-500/20 dark:bg-blue-300/20 dark:text-blue-300 dark:hover:bg-blue-300/10"
-          >
-            {creating ? creatingLabel : newDocumentLabel}{' '}
-            {creating ? (
-              <Loader className="ml-1 h-4 w-4" />
-            ) : (
-              <DocumentPlusIcon className="h-4 w-4" />
-            )}
-          </button>
-          <div className="flex gap-2 rounded-lg border border-zinc-300 p-2 dark:border-zinc-800">
-            <button
-              className={`${
-                mode === 'list'
-                  ? 'bg-zinc-500/20 dark:bg-zinc-800'
-                  : 'text-zinc-700'
-              } h-fit rounded-lg p-2 transition`}
-              onClick={() => setMode('list')}
-            >
-              <ListBulletIcon className="h-6 w-6" />
-            </button>
-            <button
-              className={`${
-                mode === 'grid'
-                  ? 'bg-zinc-500/20 dark:bg-zinc-800'
-                  : 'text-zinc-700'
-              } h-fit rounded-lg p-2 transition`}
-              onClick={() => setMode('grid')}
-            >
-              <Squares2X2Icon className="h-6 w-6" />
-            </button>
-          </div>
-        </div>
+        <button
+          // onClick={showDocumentEditForm}
+          className="flex flex-none items-center gap-1 rounded bg-blue-500/10 p-4 font-semibold text-blue-600 transition hover:bg-blue-500/20 dark:bg-blue-300/20 dark:text-blue-300 dark:hover:bg-blue-300/10"
+        >
+          {newDocumentLabel} <DocumentPlusIcon className="h-4 w-4" />
+        </button>
       </div>
 
-      <Divider className="my-4" />
+      <Separator className="my-4" />
 
-      <div
-        className={`grid ${
-          mode === 'list'
-            ? 'grid-cols-1'
-            : 'md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4'
-        } mt-2 gap-4`}
-      >
+      <div className="mt-2 grid gap-4 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4">
         {documents && documents.length === 0 && (
           <div className="col-span-full text-zinc-500">{noDocumentsLabel}</div>
         )}
 
         {ws &&
           documents &&
-          documents?.map((doc) => (
-            <DocumentCard
-              key={`doc-${doc.id}`}
-              wsId={ws?.id}
-              document={doc}
-              mode={mode}
-            />
+          documents?.map((doc: Document) => (
+            <DocumentCard key={`doc-${doc.id}`} wsId={ws?.id} document={doc} />
           ))}
       </div>
     </>
   );
+}
+
+async function getDocuments(wsId: string) {
+  const supabase = createServerComponentClient({ cookies });
+
+  const { data } = await supabase
+    .from('workspace_documents')
+    .select('id, name, content, created_at')
+    .eq('ws_id', wsId)
+    .order('created_at', { ascending: false });
+
+  return data;
 }
--- apps/web/src/components/document/DocumentCard.tsx
+++ apps/web/src/components/document/DocumentCard.tsx
@@ -1,18 +1,17 @@
 import Link from 'next/link';
 import { Document } from '../../types/primitives/Document';
-import { Divider } from '@mantine/core';
 import moment from 'moment';
 import { DocumentPlusIcon } from '@heroicons/react/24/solid';
 import useTranslation from 'next-translate/useTranslation';
 import 'moment/locale/vi';
+import { Separator } from '../ui/separator';
 
 interface Props {
   wsId: string;
   document: Document;
-  mode: 'list' | 'grid';
 }
 
-const DocumentCard = ({ wsId, document, mode }: Props) => {
+const DocumentCard = ({ wsId, document }: Props) => {
   const { id, name, content, created_at } = document;
   const href = id ? `/${wsId}/documents/${id}` : '';
 
@@ -24,9 +23,7 @@ const DocumentCard = ({ wsId, document, mode }: Props) => {
     <Link
       href={href}
       key={`doc-${id}`}
-      className={`flex ${
-        mode === 'list' ? 'items-center gap-4' : 'flex-col'
-      } relative flex justify-between rounded-lg border border-zinc-300 bg-zinc-500/5 p-4 transition hover:bg-zinc-500/10 dark:border-zinc-800/80 dark:bg-zinc-900 dark:hover:bg-zinc-800/80`}
+      className="relative flex flex items-center justify-between gap-4 rounded-lg border border-zinc-300 bg-zinc-500/5 p-4 transition hover:bg-zinc-500/10 dark:border-zinc-800/80 dark:bg-zinc-900 dark:hover:bg-zinc-800/80"
     >
       <div>
         <p className="line-clamp-1 font-semibold lg:text-lg xl:text-xl">
@@ -35,7 +32,7 @@ const DocumentCard = ({ wsId, document, mode }: Props) => {
 
         {content && (
           <>
-            <Divider className="my-2" />
+            <Separator className="my-2" />
 
             <p className="text-zinc-700 dark:text-zinc-400">
               <div
@@ -49,11 +46,7 @@ const DocumentCard = ({ wsId, document, mode }: Props) => {
         )}
       </div>
 
-      <div
-        className={`${
-          mode === 'grid' && 'mt-8'
-        } flex flex-wrap items-center gap-2 justify-self-end text-sm font-semibold`}
-      >
+      <div className="mt-8 flex flex-wrap items-center gap-2 justify-self-end text-sm font-semibold">
         <div className="flex max-w-full items-center gap-1 rounded-lg bg-blue-500/10 px-2 py-1.5 text-blue-600 dark:bg-blue-300/10 dark:text-blue-300">
           <DocumentPlusIcon className="w-5 flex-none" />
           <div className="line-clamp-1">{creationDate}</div>
