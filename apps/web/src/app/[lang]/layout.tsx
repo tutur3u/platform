b9db984c1cc23b5b43ba34700e3ac8287cffb36a
Võ Hoàng Phúc <contact@vohoangphuc.com>
2023-10-13 12:50:19 +0700

    feat: add dynamic metadata for maketing page

--- apps/web/src/app/[lang]/layout.tsx
+++ apps/web/src/app/[lang]/layout.tsx
@@ -9,67 +9,81 @@ import { Metadata } from 'next';
 import { Analytics as VercelAnalytics } from '@vercel/analytics/react';
 import GoogleAnalytics from '@/components/google-analytics';
 
-export const metadata: Metadata = {
-  title: {
-    default: siteConfig.name,
-    template: `%s - ${siteConfig.name}`,
-  },
-  description: siteConfig.description,
-  keywords: [
-    'Next.js',
-    'React',
-    'Tailwind CSS',
-    'Server Components',
-    'Radix UI',
-  ],
-  authors: [
-    {
-      name: 'vohoangphuc',
-      url: 'https://www.vohoangphuc.com',
-    },
-  ],
-  creator: 'vohoangphuc',
-  themeColor: [
-    { media: '(prefers-color-scheme: light)', color: 'white' },
-    { media: '(prefers-color-scheme: dark)', color: 'black' },
-  ],
-  openGraph: {
-    type: 'website',
-    locale: 'en_US',
-    url: siteConfig.url,
-    title: siteConfig.name,
-    description: siteConfig.description,
-    siteName: siteConfig.name,
-    images: [
-      {
-        url: siteConfig.ogImage,
-        width: 1200,
-        height: 630,
-        alt: siteConfig.name,
-      },
-    ],
-  },
-  twitter: {
-    card: 'summary_large_image',
-    title: siteConfig.name,
-    description: siteConfig.description,
-    images: [siteConfig.ogImage],
-    creator: '@tutur3u',
-  },
-  viewport:
-    'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=yes, shrink-to-fit=no',
-  icons: {
-    icon: '/favicon.ico',
-    shortcut: '/favicon-16x16.png',
-    apple: '/apple-touch-icon.png',
-  },
-  manifest: `${siteConfig.url}/site.webmanifest`,
-};
-
 interface Props {
   children: ReactNode;
   params: {
     lang: string;
+  };
+}
+
+export async function generateMetadata({
+  params: { lang },
+}: Props): Promise<Metadata> {
+  const enDescription = 'Take control of your workflow, supercharged by AI.';
+  const viDescription = 'Quản lý công việc của bạn, siêu tốc độ cùng AI.';
+
+  const description =
+    lang === 'en'
+      ? enDescription
+      : lang === 'vi'
+      ? viDescription
+      : enDescription;
+
+  return {
+    title: {
+      default: siteConfig.name,
+      template: `%s - ${siteConfig.name}`,
+    },
+    description,
+    keywords: [
+      'Next.js',
+      'React',
+      'Tailwind CSS',
+      'Server Components',
+      'Radix UI',
+    ],
+    authors: [
+      {
+        name: 'vohoangphuc',
+        url: 'https://www.vohoangphuc.com',
+      },
+    ],
+    creator: 'vohoangphuc',
+    themeColor: [
+      { media: '(prefers-color-scheme: light)', color: 'white' },
+      { media: '(prefers-color-scheme: dark)', color: 'black' },
+    ],
+    openGraph: {
+      type: 'website',
+      locale: 'en_US',
+      url: siteConfig.url,
+      title: siteConfig.name,
+      description,
+      siteName: siteConfig.name,
+      images: [
+        {
+          url: siteConfig.ogImage,
+          width: 1200,
+          height: 630,
+          alt: siteConfig.name,
+        },
+      ],
+    },
+    twitter: {
+      card: 'summary_large_image',
+      title: siteConfig.name,
+      description,
+      images: [siteConfig.ogImage],
+      creator: '@tutur3u',
+    },
+    viewport:
+      'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=yes, shrink-to-fit=no',
+    icons: {
+      icon: '/favicon.ico',
+      shortcut: '/favicon-16x16.png',
+      apple: '/apple-touch-icon.png',
+    },
+    manifest: `${siteConfig.url}/site.webmanifest`,
   };
 }
 
