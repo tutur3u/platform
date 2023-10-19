4890a91434da1f1100b63a0d12dee9410053f0ce
Võ Hoàng Phúc <contact@vohoangphuc.com>
2023-10-19 22:02:24 +0700

    feat(local-dev): add tailwind indicator preview

---
+++ apps/web/src/components/tailwind-indicator.tsx
@@ -0,0 +1,16 @@
+import { HIDE_TAILWIND_INDICATOR, PROD_MODE } from '@/constants/common';
+
+export function TailwindIndicator() {
+  if (HIDE_TAILWIND_INDICATOR || PROD_MODE) return null;
+
+  return (
+    <div className="bg-foreground text-background fixed bottom-2 left-2 z-50 flex h-8 w-8 items-center justify-center rounded-lg font-mono text-xs">
+      <div className="block sm:hidden">xs</div>
+      <div className="hidden sm:block md:hidden">sm</div>
+      <div className="hidden md:block lg:hidden">md</div>
+      <div className="hidden lg:block xl:hidden">lg</div>
+      <div className="hidden xl:block 2xl:hidden">xl</div>
+      <div className="hidden 2xl:block">2xl</div>
+    </div>
+  );
+}
--- apps/web/src/constants/common.ts
+++ apps/web/src/constants/common.ts
@@ -2,8 +2,9 @@ import { WorkspacePreset } from '@/types/primitives/WorkspacePreset';
 import { version } from '../core/version';
 
 export const APP_VERSION = version;
+
 export const DEV_MODE = process.env.NODE_ENV === 'development';
-export const PROD_MODE = !DEV_MODE;
+export const PROD_MODE = process.env.NODE_ENV === 'production';
 
 export const BASE_URL = process.env.BASE_URL || 'http://localhost:7803';
 export const API_URL = process.env.API_URL || 'http://localhost:7803/api';
@@ -17,3 +18,7 @@ export const AI_CHAT_DISABLED_PRESETS: WorkspacePreset[] = [
 
 export const LOCALE_COOKIE_NAME = 'NEXT_LOCALE';
 export const THEME_COOKIE_NAME = 'NEXT_THEME';
+
+// The following option only works in development mode.
+export const HIDE_TAILWIND_INDICATOR =
+  process.env.HIDE_TAILWIND_INDICATOR !== 'false';
