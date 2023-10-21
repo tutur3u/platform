6cdea81fe70aaf5a5098d0f6fcd0df1b71f50cd3
Võ Hoàng Phúc <contact@vohoangphuc.com>
2023-10-21 18:07:41 +0700

    chore: add error handling for ai chat api route for debugging

--- apps/web/src/app/api/chat/route.ts
+++ apps/web/src/app/api/chat/route.ts
@@ -1,67 +1,71 @@
 import { AnthropicStream, StreamingTextResponse } from 'ai';
 import { buildPrompt } from './prompts';
 
-export const dynamic = 'force-dynamic';
 export const runtime = 'edge';
 
 export async function POST(req: Request) {
-  const { messages, previewToken } = await req.json();
+  try {
+    const { messages, previewToken } = await req.json();
 
-  if (!messages) {
-    return new Response('Missing messages', { status: 400 });
-  }
+    if (!messages) {
+      return new Response('Missing messages', { status: 400 });
+    }
 
-  const apiKey = previewToken || process.env.ANTHROPIC_API_KEY;
+    const apiKey = previewToken || process.env.ANTHROPIC_API_KEY;
 
-  if (!apiKey) {
-    return new Response('Missing API key', { status: 400 });
-  }
+    if (!apiKey) {
+      return new Response('Missing API key', { status: 400 });
+    }
 
-  const prompt = buildPrompt(messages);
-  const model = 'claude-2';
+    const prompt = buildPrompt(messages);
+    const model = 'claude-2';
 
-  const response = await fetch('https://api.anthropic.com/v1/complete', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'x-api-key': apiKey,
-    } as HeadersInit,
-    body: JSON.stringify({
-      prompt,
-      model,
-      max_tokens_to_sample: 95 * 1024,
-      temperature: 0.9,
-      stream: true,
-    }),
-  });
-
-  if (!response.ok) {
-    return new Response(await response.text(), {
-      status: response.status,
+    const response = await fetch('https://api.anthropic.com/v1/complete', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'x-api-key': apiKey,
+      } as HeadersInit,
+      body: JSON.stringify({
+        prompt,
+        model,
+        max_tokens_to_sample: 95 * 1024,
+        temperature: 0.9,
+        stream: true,
+      }),
     });
-  }
 
-  const stream = AnthropicStream(
-    response
-    // ,{
-    // onStart: async () => {
-    // This callback is called when the stream starts
-    // You can use this to save the prompt to your database
-    // await savePromptToDatabase(prompt);
-    // console.log('start');
-    // },
-    // onToken: async (token: string) => {
-    // This callback is called for each token in the stream
-    // You can use this to debug the stream or save the tokens to your database
-    // console.log('token', token);
-    // },
-    // onCompletion: async (completion: string) => {
-    // This callback is called when the completion is ready
-    // You can use this to save the final completion to your database
-    // await saveCompletionToDatabase(completion);
-    // console.log(completion);
-    // },
-    // }
-  );
-  return new StreamingTextResponse(stream);
+    if (!response.ok) {
+      return new Response(await response.text(), {
+        status: response.status,
+      });
+    }
+
+    const stream = AnthropicStream(
+      response
+      // ,{
+      // onStart: async () => {
+      // This callback is called when the stream starts
+      // You can use this to save the prompt to your database
+      // await savePromptToDatabase(prompt);
+      // console.log('start');
+      // },
+      // onToken: async (token: string) => {
+      // This callback is called for each token in the stream
+      // You can use this to debug the stream or save the tokens to your database
+      // console.log('token', token);
+      // },
+      // onCompletion: async (completion: string) => {
+      // This callback is called when the completion is ready
+      // You can use this to save the final completion to your database
+      // await saveCompletionToDatabase(completion);
+      // console.log(completion);
+      // },
+      // }
+    );
+    return new StreamingTextResponse(stream);
+  } catch (e: any) {
+    console.error(e);
+    return new Response(e?.toString(), { status: 500 });
+  }
 }
