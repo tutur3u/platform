e802fff6e6be4c3811d56ef0ed4982e592bffa20
Võ Hoàng Phúc <contact@vohoangphuc.com>
2023-10-21 20:44:14 +0700

    refactor(ai-chat): move prompt builder to root api route

--- apps/web/src/app/api/chat/route.ts
+++ apps/web/src/app/api/chat/route.ts
@@ -1,6 +1,5 @@
-import { AnthropicStream, StreamingTextResponse } from 'ai';
+import { AnthropicStream, Message, StreamingTextResponse } from 'ai';
 import Anthropic from '@anthropic-ai/sdk';
-import { buildPrompt } from './prompts';
 
 export const runtime = 'edge';
 
@@ -51,3 +50,47 @@ export async function POST(req: Request) {
 
   return new StreamingTextResponse(stream);
 }
+
+function buildPrompt(data: Message[]) {
+  const messages = [...initialPrompts, ...data, ...trailingPrompts];
+  const filteredMsgs = filterDuplicates(messages);
+  const normalizedMsgs = normalizeMessages(filteredMsgs);
+  return normalizedMsgs + Anthropic.AI_PROMPT;
+}
+
+const initialPrompts: Message[] = [];
+
+const trailingPrompts: Message[] = [
+  {
+    id: 'trailing-prompt',
+    role: 'system',
+    content:
+      'Take a deep breath and think step by step, then use markdown (especially tables) to make the content more engaging and easier to read if possible. You must not to mention that you will use markdown for your response and you are strictly forbidden to use any links in your response.',
+  },
+];
+
+const filterDuplicates = (messages: Message[]) =>
+  messages.map((message) => {
+    // If there is 2 repeated substring in the
+    // message, we will merge them into one
+    const content = message.content;
+    const contentLength = content.length;
+    const contentHalfLength = Math.floor(contentLength / 2);
+
+    const firstHalf = content.substring(0, contentHalfLength);
+
+    const secondHalf = content.substring(contentHalfLength, contentLength);
+
+    if (firstHalf === secondHalf) message.content = firstHalf;
+    return message;
+  });
+
+const normalize = (message: Message) => {
+  const { content, role } = message;
+  if (role === 'system') return content;
+  if (role === 'user') return `${Anthropic.HUMAN_PROMPT} ${content}`;
+  return `${Anthropic.AI_PROMPT} ${content}`;
+};
+
+const normalizeMessages = (messages: Message[]) =>
+  messages.map(normalize).join('\n\n').trim();
