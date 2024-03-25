70543e4f6a91597c127a540d53fc9f768d4b3ae7
Võ Hoàng Phúc <contact@vohoangphuc.com>
2024-03-25 11:05:51 +0700

    chore: update og-image

--- apps/web/src/constants/configs.ts
+++ apps/web/src/constants/configs.ts
@@ -4,8 +4,8 @@ export const siteConfig = {
   name: 'RMIT Neo Culture Tech',
   url: DEV_MODE ? 'http://localhost:7803' : 'https://nct.vohoangphuc.com',
   ogImage: DEV_MODE
-    ? 'http://localhost:7803/media/logos/og-image.jpg'
-    : 'https://nct.vohoangphuc.com/media/logos/og-image.jpg',
+    ? 'http://localhost:7803/media/logos/og-image.png'
+    : 'https://nct.vohoangphuc.com/media/logos/og-image.png',
   links: {
     twitter: 'https://twitter.com/tutur3u',
     github: 'https://github.com/tutur3u/tutur3u',
