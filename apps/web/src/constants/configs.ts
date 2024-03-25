39a3d75351e0b09ff13cda9187108b3368ae3450
Võ Hoàng Phúc <contact@vohoangphuc.com>
2024-03-25 10:37:28 +0700

    feat: update og image and website description for NCT

--- apps/web/src/constants/configs.ts
+++ apps/web/src/constants/configs.ts
@@ -4,8 +4,8 @@ export const siteConfig = {
   name: 'RMIT Neo Culture Tech',
   url: DEV_MODE ? 'http://localhost:7803' : 'https://tuturuuu.com',
   ogImage: DEV_MODE
-    ? 'http://localhost:7803/media/logos/og-image.png'
-    : 'https://tuturuuu.com/media/logos/og-image.png',
+    ? 'http://localhost:7803/media/logos/og-image.jpg'
+    : 'https://tuturuuu.com/media/logos/og-image.jpg',
   links: {
     twitter: 'https://twitter.com/tutur3u',
     github: 'https://github.com/tutur3u/tutur3u',
