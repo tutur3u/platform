e84e9606c656a8c47b61a5287ba7979f0ed188fe
Võ Hoàng Phúc <vohoangphuc@tuturuuu.com>
2024-04-04 14:21:39 +0700

    feat: add docs powered by mintlify (#1339)

--- apps/web/src/app/[lang]/navlinks.tsx
+++ apps/web/src/app/[lang]/navlinks.tsx
@@ -17,12 +17,18 @@ export default function Navlinks() {
     return null;
 
   return (
-    <div className="left-1/2 top-1/2 hidden -translate-x-1/2 -translate-y-1/2 transform items-center justify-center gap-4 md:absolute md:grid">
+    <div className="left-1/2 top-1/2 hidden -translate-x-1/2 -translate-y-1/2 transform items-center justify-center gap-4 md:absolute md:flex">
       <Link
         href="/calendar/meet-together"
         className="opacity-50 hover:opacity-100"
       >
         {t('meet-together')}
+      </Link>
+      <Link
+        href="https://docs.tuturuuu.com"
+        className="opacity-50 hover:opacity-100"
+      >
+        {t('docs')}
       </Link>
     </div>
   );
