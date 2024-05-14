735ec2c7ca069fc65487baa9f11ac34df2929f72
Võ Hoàng Phúc <contact@vohoangphuc.com>
2024-05-14 15:56:41 +0700

    Revert "feat!: add root-level layout and update lang-level layout"
    This reverts commit 0feedf5c88e634a2920eb150b3a4007f46d70639.

--- apps/web/src/app/[lang]/layout.tsx
+++ apps/web/src/app/[lang]/layout.tsx
@@ -1,14 +1,18 @@
+import '../../styles/globals.css';
 import { ReactNode } from 'react';
 
+import { Analytics as VercelAnalytics } from '@vercel/analytics/react';
+import { SpeedInsights as VercelInsights } from '@vercel/speed-insights/next';
 import { TailwindIndicator } from '@/components/tailwind-indicator';
 import { Providers } from '@/components/providers';
 import { Toaster } from '@/components/ui/toaster';
 import { siteConfig } from '@/constants/configs';
 import { StaffToolbar } from './staff-toolbar';
 import NavbarPadding from './navbar-padding';
-import { Metadata } from 'next';
+import { Metadata, Viewport } from 'next';
+import { Inter } from 'next/font/google';
+import { cn } from '@/lib/utils';
 import Navbar from './navbar';
-import Head from 'next/head';
 
 export const dynamic = 'force-dynamic';
 
@@ -80,44 +84,66 @@ export async function generateMetadata({
   };
 }
 
-export default async function RootLayout({
-  children,
-  params: { lang },
-}: Props) {
+export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  maximumScale: 5,
+  themeColor: [
+    { media: '(prefers-color-scheme: dark)', color: 'black' },
+    { media: '(prefers-color-scheme: light)', color: 'white' },
+  ],
+  colorScheme: 'dark light',
+};
+
+const inter = Inter({ subsets: ['latin', 'vietnamese'], display: 'block' });
+
+export async function generateStaticParams() {
+  return [{ lang: 'en' }, { lang: 'vi' }];
+}
+
+export default async function RootLayout({ children, params }: Props) {
   return (
-    <Head>
-      <html lang={lang} />
-      <Providers
-        attribute="class"
-        defaultTheme="dark"
-        themes={[
-          'system',
-
-          'light',
-          'light-pink',
-          'light-purple',
-          'light-yellow',
-          'light-orange',
-          'light-green',
-          'light-blue',
-
-          'dark',
-          'dark-pink',
-          'dark-purple',
-          'dark-yellow',
-          'dark-orange',
-          'dark-green',
-          'dark-blue',
-        ]}
-        enableColorScheme={false}
-        enableSystem
+    <html lang={params.lang}>
+      <body
+        className={cn(
+          'bg-background min-h-screen font-sans antialiased',
+          inter.className
+        )}
       >
-        <Navbar />
-        <NavbarPadding>{children}</NavbarPadding>
-      </Providers>
-      <TailwindIndicator />
-      <StaffToolbar />
-      <Toaster />
-    </Head>
+        <VercelAnalytics />
+        <VercelInsights />
+        <Providers
+          attribute="class"
+          defaultTheme="dark"
+          themes={[
+            'system',
+
+            'light',
+            'light-pink',
+            'light-purple',
+            'light-yellow',
+            'light-orange',
+            'light-green',
+            'light-blue',
+
+            'dark',
+            'dark-pink',
+            'dark-purple',
+            'dark-yellow',
+            'dark-orange',
+            'dark-green',
+            'dark-blue',
+          ]}
+          enableColorScheme={false}
+          enableSystem
+        >
+          <Navbar />
+          <NavbarPadding>{children}</NavbarPadding>
+        </Providers>
+        <TailwindIndicator />
+        <StaffToolbar />
+        <Toaster />
+      </body>
+    </html>
   );
 }
