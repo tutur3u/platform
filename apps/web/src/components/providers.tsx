21fda1305de1dcc9963eb0cad2ee5881877f07ac
Võ Hoàng Phúc <contact@vohoangphuc.com>
2024-05-30 15:57:45 +0700

    feat: split client-side providers and server-side providers

--- apps/web/src/components/providers.tsx
+++ apps/web/src/components/providers.tsx
@@ -1,21 +1,11 @@
-'use client';
-
-import * as React from 'react';
-import { ThemeProvider as NextThemesProvider } from 'next-themes';
+import { ClientProviders } from './client-providers';
+import { ThemeProvider } from 'next-themes';
 import { ThemeProviderProps } from 'next-themes/dist/types';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import { TooltipProvider } from '@/components/ui/tooltip';
 
-const queryClient = new QueryClient();
-
-export function Providers({ children, ...props }: ThemeProviderProps) {
+export function Providers({ children }: ThemeProviderProps) {
   return (
-    <NextThemesProvider {...props}>
-      <QueryClientProvider client={queryClient}>
-        <TooltipProvider>{children}</TooltipProvider>
-        <ReactQueryDevtools initialIsOpen={false} />
-      </QueryClientProvider>
-    </NextThemesProvider>
+    <ThemeProvider attribute="class">
+      <ClientProviders>{children}</ClientProviders>
+    </ThemeProvider>
   );
 }
