2fbaa0c3ef63ddc2a81deb4a29056abf546b2e6d
Võ Hoàng Phúc <contact@vohoangphuc.com>
2024-05-30 16:10:26 +0700

    fix(theme); correctly pass theme props to server-side providers

--- apps/web/src/components/providers.tsx
+++ apps/web/src/components/providers.tsx
@@ -2,9 +2,9 @@ import { ClientProviders } from './client-providers';
 import { ThemeProvider } from 'next-themes';
 import { ThemeProviderProps } from 'next-themes/dist/types';
 
-export function Providers({ children }: ThemeProviderProps) {
+export function Providers({ children, ...props }: ThemeProviderProps) {
   return (
-    <ThemeProvider attribute="class">
+    <ThemeProvider {...props}>
       <ClientProviders>{children}</ClientProviders>
     </ThemeProvider>
   );
