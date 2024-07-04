478c77766ca56db133d8141a4a2aa12d37d9f575
Võ Hoàng Phúc <contact@vohoangphuc.com>
2024-07-04 23:20:27 +0700

    chore: add test candy crush clone game

--- apps/web/src/app/[locale]/(marketing)/neo-crush/page.tsx
+++ apps/web/src/app/[locale]/(marketing)/neo-crush/page.tsx
@@ -1,7 +1,9 @@
+import CandyCrushClone from './test';
+
 export default function NeoCrushGame() {
   return (
     <div className="flex min-h-full items-center justify-center text-2xl font-semibold 2xl:text-4xl">
-      Neo Crush Game
+      <CandyCrushClone />
     </div>
   );
 }
