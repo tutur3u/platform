1df91c37d0dca589071ea74dc1699d47ef40f8a0
Võ Hoàng Phúc <contact@vohoangphuc.com>
2024-07-06 12:02:41 +0700

    chore: add lobby & leaderboard placeholder pages

---
+++ apps/web/src/app/[locale]/(games)/neo-crush/leaderboard/page.tsx
@@ -0,0 +1,3 @@
+export default function NeoCrushLeaderboardPage() {
+  return <div></div>;
+}
--- apps/web/src/app/[locale]/(games)/neo-crush/page.tsx
+++ apps/web/src/app/[locale]/(games)/neo-crush/page.tsx
@@ -4,8 +4,16 @@ import { NeoCrushGame } from './game';
 
 export default function NeoCrushGamePage() {
   return (
-    <div className="flex items-center justify-center p-4 pt-8 md:min-h-full md:pt-32">
+    <div className="flex flex-col items-center justify-center gap-4 p-4 pt-8 md:min-h-full md:flex-row md:pt-32">
+      {/* <div className="bg-foreground/5 rounded border p-4">
+        <div className="font-semibold">Võ Hoàng Phúc</div>
+        <div>s3926761@rmit.edu.vn</div>
+      </div> */}
       <NeoCrushGame />
+      {/* <div className="bg-foreground/5 rounded border p-4">
+        <div className="font-semibold">Võ Hoàng Phúc</div>
+        <div>s3926761@rmit.edu.vn</div>
+      </div> */}
     </div>
   );
 }
