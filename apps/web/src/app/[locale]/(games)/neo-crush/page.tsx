c5b350891bb1fa99421424817f2e6ffee91967c7
Võ Hoàng Phúc <contact@vohoangphuc.com>
2024-07-07 11:35:28 +0700

    fix: lock page height to prevent overflow scrolling

--- apps/web/src/app/[locale]/(games)/neo-crush/page.tsx
+++ apps/web/src/app/[locale]/(games)/neo-crush/page.tsx
@@ -4,7 +4,7 @@ import { NeoCrushGame } from './game';
 
 export default function NeoCrushGamePage() {
   return (
-    <div className="flex flex-col items-center justify-center gap-4 p-4 pt-8 md:min-h-full md:flex-row md:pt-32">
+    <div className="flex h-fit flex-col items-center justify-center gap-4 overflow-hidden p-4 pt-8 md:flex-row md:pt-32">
       {/* <div className="bg-foreground/5 rounded border p-4">
         <div className="font-semibold">Võ Hoàng Phúc</div>
         <div>s3926761@rmit.edu.vn</div>
