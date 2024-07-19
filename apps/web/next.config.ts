df67138dd823cf119619b24c948ba5d2ae0fa6db
Võ Hoàng Phúc <contact@vohoangphuc.com>
2024-07-19 13:23:02 +0700

    chore: convert next.config.mjs to next.config.ts for better types

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,9 +1,9 @@
+import { NextConfig } from 'next';
 import createNextIntlPlugin from 'next-intl/plugin';
 
 const withNextIntl = createNextIntlPlugin();
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig: NextConfig = {
   reactStrictMode: true,
   poweredByHeader: false,
   experimental: {
