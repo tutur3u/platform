dee5dc0a42ddfba83233516f4e75c26c62c0128d
Võ Hoàng Phúc <contact@vohoangphuc.com>
2024-07-23 19:19:03 +0700

    fix: remove unstable_noStore and try/catch for generateMetadata logic

--- apps/web/src/app/[locale]/(marketing)/ai/chats/[chatId]/layout.tsx
+++ apps/web/src/app/[locale]/(marketing)/ai/chats/[chatId]/layout.tsx
@@ -2,7 +2,6 @@ import { siteConfig } from '@/constants/configs';
 import { AIChat } from '@/types/db';
 import { createAdminClient } from '@/utils/supabase/server';
 import { Metadata } from 'next';
-import { unstable_noStore } from 'next/cache';
 import { notFound } from 'next/navigation';
 import { ReactNode } from 'react';
 
@@ -36,62 +35,56 @@ const getChat = async (chatId: string) => {
 export const generateMetadata = async ({
   params: { locale, chatId },
 }: Props): Promise<Metadata> => {
-  unstable_noStore();
-  try {
-    const viTitle = 'Trò chuyện AI';
-    const enTitle = 'AI Chat';
+  const viTitle = 'Trò chuyện AI';
+  const enTitle = 'AI Chat';
 
-    const enDefaultDescription =
-      'Discuss with AI about anything, anytime, anywhere.';
-    const viDefaultDescription =
-      'Trò chuyện với AI về mọi thứ, mọi lúc, mọi nơi.';
+  const enDefaultDescription =
+    'Discuss with AI about anything, anytime, anywhere.';
+  const viDefaultDescription =
+    'Trò chuyện với AI về mọi thứ, mọi lúc, mọi nơi.';
 
-    const untitled = locale === 'vi' ? 'Chưa đặt tên' : 'Untitled';
-    const defaultDescription =
-      locale === 'vi' ? viDefaultDescription : enDefaultDescription;
+  const untitled = locale === 'vi' ? 'Chưa đặt tên' : 'Untitled';
+  const defaultDescription =
+    locale === 'vi' ? viDefaultDescription : enDefaultDescription;
 
-    const chat = await getChat(chatId);
+  const chat = await getChat(chatId);
 
-    const chatTitle = chat.title || untitled;
-    const chatSummary = chat.summary || defaultDescription;
+  const chatTitle = chat.title || untitled;
+  const chatSummary = chat.summary || defaultDescription;
 
-    const title = `${chatTitle} - ${locale === 'vi' ? viTitle : enTitle}`;
-    const description = chatSummary;
+  const title = `${chatTitle} - ${locale === 'vi' ? viTitle : enTitle}`;
+  const description = chatSummary;
 
-    return {
-      title: {
-        default: title,
-        template: `%s - ${title}`,
-      },
+  return {
+    title: {
+      default: title,
+      template: `%s - ${title}`,
+    },
+    description,
+    openGraph: {
+      type: 'website',
+      locale,
+      url: siteConfig.url,
+      title,
       description,
-      openGraph: {
-        type: 'website',
-        locale,
-        url: siteConfig.url,
-        title,
-        description,
-        siteName: siteConfig.name,
-        images: [
-          {
-            url: siteConfig.ogImage,
-            width: 1200,
-            height: 630,
-            alt: `${title} - ${siteConfig.name}`,
-          },
-        ],
-      },
-      twitter: {
-        card: 'summary_large_image',
-        title,
-        description,
-        images: [siteConfig.ogImage],
-        creator: '@tutur3u',
-      },
-    };
-  } catch (error) {
-    console.error(error);
-    notFound();
-  }
+      siteName: siteConfig.name,
+      images: [
+        {
+          url: siteConfig.ogImage,
+          width: 1200,
+          height: 630,
+          alt: `${title} - ${siteConfig.name}`,
+        },
+      ],
+    },
+    twitter: {
+      card: 'summary_large_image',
+      title,
+      description,
+      images: [siteConfig.ogImage],
+      creator: '@tutur3u',
+    },
+  };
 };
 
 export default async function AIChatDetailsLayout({
