4c47f2356bc42706b2f4a39eba0b1ff6cc25b775
Luong Ngoc Bao Tran <s4031723@rmit.edu.vn>
2024-08-23 18:03:29 +0700

    feat: implement board

--- apps/web/src/app/[locale]/(games)/neo-chess/chessboard.tsx
+++ apps/web/src/app/[locale]/(games)/neo-chess/chessboard.tsx
@@ -1,40 +1,42 @@
 import React from 'react';
 
-const horizontal: string[] = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
-const vertical: number[] = [1, 2, 3, 4, 5, 6, 7, 8];
+const horizontal = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+const vertical = [1, 2, 3, 4, 5, 6, 7, 8];
 
 export default function ChessBoard() {
     let board: React.ReactNode[] = [];
     
-    for (let i = 0; i <= horizontal.length; i++) {
+    for (let i = 0; i <= horizontal.length + 1; i++) {
         const row: React.ReactNode[] = [];
-        for (let j = 0; j <= vertical.length; j++) {
-            if (i === 0) {
-                if (j === 0) {
+        for (let j = 0; j <= vertical.length + 1; j++) {
+            if (i === 0 || i === horizontal.length + 1) {
+                if (j === 0 || j === vertical.length + 1) {
                     row.push(
-                        <div className='square bg-inherit w-20 sm:w-16 aspect-square'></div>
+                        <div key={null} className='relative flex h-8 aspect-square items-center justify-center md:h-9 md:w-9 lg:h-10 lg:w-10'></div>
                     )
                 } else {
                     row.push(
-                        <div className='square p-1 bg-inherit w-20 sm:w-16 aspect-square text-center flex items-end justify-center'>
-                            {`${horizontal[j - 1]}`}
+                        <div key={j-1} className='relative flex h-8 aspect-square items-center justify-center md:h-9 md:w-9 lg:h-10 lg:w-10'>
+                            {`${horizontal[j-1]}`}
                         </div>
                     )
                 }                
             } else {
-                if (j === 0) {
+                if (j === 0 || j === vertical.length + 1) {
                     row.push(
-                        <div className='square p-1 bg-inherit w-20 sm:w-16 aspect-square text-right flex justify-center'>
-                            {`${vertical[i - 1]}`}
+                        <div key={i-1} className='relative flex h-8 aspect-square items-center justify-center md:h-9 md:w-9 lg:h-10 lg:w-10'>
+                            {`${vertical[vertical.length - i]}`}
                         </div>
                     )
                 } else {
                     row.push(
                         (i%2!==0 && j%2!==0 || i%2===0 && j%2===0) ? (
-                            <div className='square bg-white w-20 sm:w-16 aspect-square'>
+                            <div className='relative flex h-8 aspect-square items-center justify-center md:h-9 md:w-9 lg:h-10 lg:w-10'>
+                                x
                             </div>
                         ) : (
-                            <div className='square bg-blue-200 w-20 sm:w-16 aspect-square'>
+                            <div className='relative bg-blue-200 flex h-8 aspect-square items-center justify-center md:h-9 md:w-9 lg:h-10 lg:w-10'>
+                                y
                             </div>
                         )
                     );
@@ -43,15 +45,21 @@ export default function ChessBoard() {
         }
 
         board.push(
-            <div className='row flex w-full h-full'>
+            <div className='mx-auto grid w-fit grid-cols-10'>
                 {row}
             </div>
         );
     }
 
     return (
-        <div className="m-auto w-9/12 h-4/5">
-            {board}
+        <div className="m-auto left-[50%] top-[50%] grid w-full p-6 sm:rounded-lg max-w-sm md:max-w-4xl lg:max-w-6xl">
+            <div className='grid grid-cols-1 md:grid-cols-2'>
+                <div className='bg-card text-card-foreground rounded-lg border shadow-sm w-full p-2 md:p-4'>
+                    <div className='relative'>
+                        {board}
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
