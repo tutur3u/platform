8f29b101ade586af9f66961cc4dccf5d774d90c4
Nguyen Do <khoanguyen.do@outlook.com>
2024-10-11 20:41:29 +0700

    feat(primitives): add empty folder placeholder constant

--- apps/web/src/types/primitives/StorageObject.ts
+++ apps/web/src/types/primitives/StorageObject.ts
@@ -1,3 +1,9 @@
+// same usage as Supabase Studio defined value
+// see: https://github.com/supabase/supabase/blob/0ba764c13fb8205b0b531e78d70d40255cb71335/apps/studio/localStores/storageExplorer/StorageExplorerStore.tsx#L74
+export const EMPTY_FOLDER_PLACEHOLDER_NAME = '.emptyFolderPlaceholder';
+
+export type StorageObjectType = 'file' | 'folder';
+
 export interface StorageObject {
   id?: string;
   bucket_id?: string;
