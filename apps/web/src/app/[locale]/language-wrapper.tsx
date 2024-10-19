a00070bf6be223e770bda3c98f488f0380095244
Võ Hoàng Phúc <contact@vohoangphuc.com>
2024-10-19 22:51:15 +0700

    fix(i18n): use next-intl canary@latest to adapt to new nextjs params promise

--- apps/web/src/app/[locale]/language-wrapper.tsx
+++ apps/web/src/app/[locale]/language-wrapper.tsx
@@ -1,14 +1,14 @@
 import { LanguageToggle } from './language-toggle';
-import { defaultLocale, locales } from '@/config';
 import { LOCALE_COOKIE_NAME } from '@/constants/common';
+import { Locale, defaultLocale, supportedLocales } from '@/i18n/routing';
 import { cookies as c } from 'next/headers';
 
 export async function LanguageWrapper() {
   const cookies = await c();
   const currentLocale = cookies.get(LOCALE_COOKIE_NAME)?.value;
-  const locale = locales.includes(currentLocale as any)
+  const locale = supportedLocales.includes(currentLocale as any)
     ? currentLocale
     : defaultLocale;
 
-  return <LanguageToggle currentLocale={locale as (typeof locales)[number]} />;
+  return <LanguageToggle currentLocale={locale as Locale} />;
 }
