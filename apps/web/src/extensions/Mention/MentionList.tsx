c63aefbfe767f064592b8d052315c4123f4c7d58
phatgg221 <129394719+phatgg221@users.noreply.github.com>
2024-11-29 14:56:17 +0700

    Update MentionList.tsx

--- apps/web/src/extensions/Mention/MentionList.tsx
+++ apps/web/src/extensions/Mention/MentionList.tsx
@@ -1,70 +1,89 @@
-// import './MentionList.scss'
-
 import React, {
-  forwardRef, useEffect, useImperativeHandle,
+  KeyboardEvent,
+  forwardRef,
+  useEffect,
+  useImperativeHandle,
   useState,
-} from 'react'
+} from 'react';
 
-export default forwardRef((props, ref) => {
-  const [selectedIndex, setSelectedIndex] = useState(0)
+interface MentionListProps {
+  items: string[];
+  command: (payload: { id: string }) => void;
+}
 
-  const selectItem = index => {
-    const item = props.items[index]
+interface MentionListRef {
+  onKeyDown: (params: { event: KeyboardEvent }) => boolean;
+}
 
-    if (item) {
-      props.command({ id: item })
-    }
-  }
+const MentionList = forwardRef<MentionListRef, MentionListProps>(
+  (props, ref) => {
+    const [selectedIndex, setSelectedIndex] = useState<number>(0);
 
-  const upHandler = () => {
-    setSelectedIndex((selectedIndex + props.items.length - 1) % props.items.length)
-  }
+    const selectItem = (index: number) => {
+      const item = props.items[index];
 
-  const downHandler = () => {
-    setSelectedIndex((selectedIndex + 1) % props.items.length)
-  }
-
-  const enterHandler = () => {
-    selectItem(selectedIndex)
-  }
-
-  useEffect(() => setSelectedIndex(0), [props.items])
-
-  useImperativeHandle(ref, () => ({
-    onKeyDown: ({ event }) => {
-      if (event.key === 'ArrowUp') {
-        upHandler()
-        return true
+      if (item) {
+        props.command({ id: item });
       }
+    };
 
-      if (event.key === 'ArrowDown') {
-        downHandler()
-        return true
-      }
+    const upHandler = () => {
+      setSelectedIndex(
+        (selectedIndex + props.items.length - 1) % props.items.length
+      );
+    };
 
-      if (event.key === 'Enter') {
-        enterHandler()
-        return true
-      }
+    const downHandler = () => {
+      setSelectedIndex((selectedIndex + 1) % props.items.length);
+    };
 
-      return false
-    },
-  }))
+    const enterHandler = () => {
+      selectItem(selectedIndex);
+    };
 
-  return (
-    <div className="bg-white border border-gray-300 rounded-lg shadow-md flex flex-col gap-1 overflow-auto p-2.5 relative">
-      {props.items.length
-        ? props.items.map((item, index) => (
-          <button
-            className={`w-full flex items-center gap-1.5 text-left ${index === selectedIndex ? 'bg-gray-200' : ''} hover:bg-gray-100`}
-            key={index}
-            onClick={() => selectItem(index)}
-          >
-            {item}
-          </button>
-        ))
-        : <div className="p-2 text-gray-500">No result</div>
-      }
-    </div>
-  )
-})
+    useEffect(() => {
+      setSelectedIndex(0);
+    }, [props.items]);
+
+    useImperativeHandle(ref, () => ({
+      onKeyDown: ({ event }: { event: KeyboardEvent }) => {
+        if (event.key === 'ArrowUp') {
+          upHandler();
+          return true;
+        }
+
+        if (event.key === 'ArrowDown') {
+          downHandler();
+          return true;
+        }
+
+        if (event.key === 'Enter') {
+          enterHandler();
+          return true;
+        }
+
+        return false;
+      },
+    }));
+
+    return (
+      <div className="relative flex flex-col gap-1 overflow-auto rounded-lg border border-gray-300 bg-white p-2.5 shadow-md">
+        {props.items.length ? (
+          props.items.map((item, index) => (
+            <button
+              className={`flex w-full items-center gap-1.5 text-left ${index === selectedIndex ? 'bg-gray-200' : ''} hover:bg-gray-100`}
+              key={index}
+              onClick={() => selectItem(index)}
+            >
+              {item}
+            </button>
+          ))
+        ) : (
+          <div className="p-2 text-gray-500">No result</div>
+        )}
+      </div>
+    );
+  }
+);
+
+export default MentionList;
