fa2932a1d4c501ec0b7abe757ba7f49ccbdf2b51
Võ Hoàng Phúc <contact@vohoangphuc.com>
2024-12-04 14:48:43 +0700

    refactor(navbar): simplify scroll handling by using window.scrollY

--- apps/web/src/components/layouts/Footer.tsx
+++ apps/web/src/components/layouts/Footer.tsx
@@ -176,7 +176,7 @@ export default function Footer() {
       </div>
 
       <Separator className="bg-foreground/5 mt-8" />
-      <div className="p-4 text-center opacity-80 md:px-32 xl:px-64">
+      <div className="text-balance p-4 text-center text-sm opacity-80 md:px-32 xl:px-64">
         {t('common.copyright')}
       </div>
     </div>
