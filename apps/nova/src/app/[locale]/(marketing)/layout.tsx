ac11535cab8d2bfcee2b64016eabda755d82bf6f
Võ Hoàng Phúc <contact@vohoangphuc.com>
2024-12-18 07:36:38 +0700

    chore(deps): update dependencies

--- apps/nova/src/app/[locale]/(marketing)/layout.tsx
+++ apps/nova/src/app/[locale]/(marketing)/layout.tsx
@@ -1,15 +1,127 @@
-import React from 'react';
+import { siteConfig } from '@/constants/configs';
+import { routing, supportedLocales } from '@/i18n/routing';
+import { Toaster } from '@repo/ui/components/ui/toaster';
+import '@repo/ui/globals.css';
+import { cn } from '@repo/ui/lib/utils';
+import { Analytics as VercelAnalytics } from '@vercel/analytics/react';
+import { SpeedInsights as VercelInsights } from '@vercel/speed-insights/next';
+import { Metadata, Viewport } from 'next';
+import { NextIntlClientProvider } from 'next-intl';
+import { getMessages, setRequestLocale } from 'next-intl/server';
+import { Inter } from 'next/font/google';
+import { notFound } from 'next/navigation';
+import { ReactNode } from 'react';
 
-interface LayoutProps {
-  children: React.ReactNode;
+const font = Inter({ subsets: ['latin', 'vietnamese'], display: 'block' });
+
+interface Props {
+  children: ReactNode;
+  params: Promise<{
+    locale: string;
+  }>;
 }
 
-export default function Layout({ children }: LayoutProps) {
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const { locale } = await params;
+
+  const enDescription = 'Take control of your workflow, supercharged by AI.';
+  const viDescription = 'Quản lý công việc của bạn, siêu tốc độ cùng AI.';
+
+  const description = locale === 'vi' ? viDescription : enDescription;
+
+  return {
+    title: {
+      default: siteConfig.name,
+      template: `%s - ${siteConfig.name}`,
+    },
+    metadataBase: new URL(siteConfig.url),
+    description,
+    keywords: [
+      'Next.js',
+      'React',
+      'Tailwind CSS',
+      'Server Components',
+      'Radix UI',
+    ],
+    authors: [
+      {
+        name: 'vohoangphuc',
+        url: 'https://www.vohoangphuc.com',
+      },
+    ],
+    creator: 'vohoangphuc',
+    openGraph: {
+      type: 'website',
+      locale,
+      url: siteConfig.url,
+      title: siteConfig.name,
+      description,
+      siteName: siteConfig.name,
+      images: [
+        {
+          url: siteConfig.ogImage,
+          width: 1200,
+          height: 630,
+          alt: siteConfig.name,
+        },
+      ],
+    },
+    twitter: {
+      card: 'summary_large_image',
+      title: siteConfig.name,
+      description,
+      images: [siteConfig.ogImage],
+      creator: '@tutur3u',
+    },
+    icons: {
+      icon: '/favicon.ico',
+      shortcut: '/favicon-16x16.png',
+      apple: '/apple-touch-icon.png',
+    },
+    manifest: `/site.webmanifest`,
+  };
+}
+
+export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  maximumScale: 5,
+  themeColor: [
+    { media: '(prefers-color-scheme: dark)', color: 'black' },
+    { media: '(prefers-color-scheme: light)', color: 'white' },
+  ],
+  colorScheme: 'dark light',
+};
+
+export function generateStaticParams() {
+  return supportedLocales.map((locale) => ({ locale }));
+}
+
+export default async function RootLayout({ children, params }: Props) {
+  // Ensure that the incoming `locale` is valid
+  if (!routing.locales.includes((await params).locale as any)) {
+    notFound();
+  }
+
+  const { locale } = await params;
+  setRequestLocale(locale);
+  const messages = await getMessages();
+
   return (
-    <>
-      <div id="main-content" className="flex flex-col pt-[53px]">
-        {children}
-      </div>
-    </>
+    <html lang={locale} suppressHydrationWarning>
+      <body
+        className={cn(
+          'bg-background overflow-y-scroll antialiased',
+          font.className
+        )}
+      >
+        <VercelAnalytics />
+        <VercelInsights />
+        <NextIntlClientProvider messages={messages}>
+          {children}
+        </NextIntlClientProvider>
+        <Toaster />
+      </body>
+    </html>
   );
 }
