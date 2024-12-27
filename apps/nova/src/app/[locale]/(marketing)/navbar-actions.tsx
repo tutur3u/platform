283cff8e4e30bc1004811cc6071f0516f8457294
phatgg221 <129394719+phatgg221@users.noreply.github.com>
2024-12-27 23:08:20 +0700

    fix: language toggle

--- apps/nova/src/app/[locale]/(marketing)/navbar-actions.tsx
+++ apps/nova/src/app/[locale]/(marketing)/navbar-actions.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 // import { ThemeToggle } from './theme-toggle';
-import LanguageToggle from './language-toggle';
+import { LanguageWrapper } from './language-wrapper';
+// import LanguageToggle from './language-toggle';
 export default function NavbarActions() {
   return (
     <div className='relative'>
         <div className='flex items-center gap-1'>
-            <LanguageToggle></LanguageToggle>
+            <LanguageWrapper></LanguageWrapper>
             {/* <ThemeToggle></ThemeToggle> */}
         </div>
     </div>
