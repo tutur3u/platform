59f774139ab4a9e2c44798c1c68ca87567ef3fdc
Võ Hoàng Phúc <contact@vohoangphuc.com>
2025-01-04 17:52:54 +0700

    feat(dashboard): add new pages for workouts, readings, diet & nutrition, progress, and metrics (wip)

---
+++ apps/web/src/app/[locale]/(dashboard)/[wsId]/workouts/page.tsx
@@ -0,0 +1,29 @@
+import GradientHeadline from '@/app/[locale]/(marketing)/gradient-headline';
+import { getPermissions, getWorkspace } from '@/lib/workspace-helper';
+import { getTranslations } from 'next-intl/server';
+import { redirect } from 'next/navigation';
+
+interface PageProps {
+  params: Promise<{
+    wsId: string;
+  }>;
+}
+
+export default async function CalendarPage({ params }: PageProps) {
+  const t = await getTranslations('common');
+  const { wsId } = await params;
+  const workspace = await getWorkspace(wsId);
+
+  const { withoutPermission } = await getPermissions({
+    wsId,
+  });
+
+  if (withoutPermission('manage_projects')) redirect(`/${wsId}`);
+  if (!workspace) return null;
+
+  return (
+    <div className="flex h-screen w-full items-center justify-center text-2xl font-bold lg:text-4xl xl:text-5xl">
+      <GradientHeadline>{t('coming_soon')} ✨</GradientHeadline>
+    </div>
+  );
+}
