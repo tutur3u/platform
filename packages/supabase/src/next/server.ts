8460a20cbebbc2f33ce863c14e96d427edd11316
Võ Hoàng Phúc <contact@vohoangphuc.com>
2025-02-10 00:14:17 +0700

    refactor(supabase): remove bundleDependencies and update cookie handler type

--- packages/supabase/src/next/server.ts
+++ packages/supabase/src/next/server.ts
@@ -1,10 +1,9 @@
 import { SupabaseCookie, checkEnvVariables } from './common';
 import { createServerClient } from '@supabase/ssr';
 import { Database } from '@tutur3u/types/supabase';
-import { ReadonlyRequestCookies } from 'next/dist/server/web/spec-extension/adapters/request-cookies';
 import { cookies } from 'next/headers';
 
-function createCookieHandler(cookieStore: ReadonlyRequestCookies) {
+function createCookieHandler(cookieStore: any) {
   return {
     getAll() {
       return cookieStore.getAll();
