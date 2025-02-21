4085e993d7f105f000dd57a451a3aac9ca909b12
Võ Hoàng Phúc <contact@vohoangphuc.com>
2025-02-21 23:44:02 +0700

    chore: remove unused chat panel and layout components

--- apps/nova/src/app/[locale]/(dashboard)/[wsId]/challenges/[challengeId]/components/chat-panel.tsx
+++
@@ -1,9 +0,0 @@
-// import React from 'react';
-// import { Model } from '@/data/models';
-// import { AIChat } from '@/types/db';
-
-// export default function ChatPanel() {
-//   return <div>
-
-//   </div>;
-// }
