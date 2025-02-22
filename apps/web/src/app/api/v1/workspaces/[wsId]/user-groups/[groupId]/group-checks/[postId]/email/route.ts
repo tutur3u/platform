08df7b6433c19610d3865315fff9d014ca0b235a
Võ Hoàng Phúc <contact@vohoangphuc.com>
2025-02-22 14:49:58 +0700

    chore: add default values for AWS region and credentials in SES client configuration

--- apps/web/src/app/api/v1/workspaces/[wsId]/user-groups/[groupId]/group-checks/[postId]/email/route.ts
+++ apps/web/src/app/api/v1/workspaces/[wsId]/user-groups/[groupId]/group-checks/[postId]/email/route.ts
@@ -18,10 +18,10 @@ const domainBlacklist = [
 ];
 
 const sesClient = new SESClient({
-  region: process.env.AWS_REGION as string,
+  region: (process.env.AWS_REGION || 'ap-southeast-1') as string,
   credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID as string,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY as string,
+    accessKeyId: (process.env.AWS_ACCESS_KEY_ID || '') as string,
+    secretAccessKey: (process.env.AWS_SECRET_ACCESS_KEY || '') as string,
   },
 });
 
