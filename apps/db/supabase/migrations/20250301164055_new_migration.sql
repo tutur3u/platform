adc8ad784f7aedf91ce4efb7f381bf901c338513
Võ Hoàng Phúc <contact@vohoangphuc.com>
2025-03-03 09:03:36 +0700

    chore(db): consolidate migration files

--- apps/db/supabase/migrations/20250301164055_new_migration.sql
+++
@@ -1,5 +0,0 @@
-alter table "public"."nova_problem_constraints" alter column "constraint_content" set not null;
-
-alter table "public"."nova_problem_testcases" alter column "testcase_content" set not null;
-
-
