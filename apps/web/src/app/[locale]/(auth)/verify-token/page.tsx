ae57f662863baa13d66ca67a00359d36f4735e13
Võ Hoàng Phúc <contact@vohoangphuc.com>
2025-03-16 03:50:20 +0700

    refactor(auth): separate token verification logic into BasicTokenVerifier component and update VerifyTokenPage to use Suspense

--- apps/web/src/app/[locale]/(auth)/verify-token/page.tsx
+++ apps/web/src/app/[locale]/(auth)/verify-token/page.tsx
@@ -1,32 +1,10 @@
-'use client';
-
-import { createClient } from '@tuturuuu/supabase/next/client';
-import { LoadingIndicator } from '@tuturuuu/ui/custom/loading-indicator';
-import { useRouter, useSearchParams } from 'next/navigation';
-import { useEffect } from 'react';
+import { BasicTokenVerifier } from './client';
+import { Suspense } from 'react';
 
 export default function VerifyTokenPage() {
-  const router = useRouter();
-  const supabase = createClient();
-  const searchParams = useSearchParams();
-
-  useEffect(() => {
-    const handleUser = async () => {
-      const {
-        data: { user },
-      } = await supabase.auth.getUser();
-
-      if (user) router.push('/onboarding');
-      else router.push('/login');
-    };
-    handleUser();
-  }, [searchParams]);
-
   return (
-    <div className="fixed inset-0 flex h-screen w-screen flex-col items-center justify-center gap-2">
-      <div className="flex h-full w-full items-center justify-center">
-        <LoadingIndicator className="h-10 w-10" />
-      </div>
-    </div>
+    <Suspense>
+      <BasicTokenVerifier />
+    </Suspense>
   );
 }
