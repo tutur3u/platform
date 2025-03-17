fca8052642ca83f39c11a52491b3e794be2c3d29
Võ Hoàng Phúc <contact@vohoangphuc.com>
2025-03-17 21:49:08 +0700

    feat(login): add GitHub login functionality and update UI components

--- apps/web/src/app/[locale]/login/page.tsx
+++ apps/web/src/app/[locale]/login/page.tsx
@@ -7,7 +7,7 @@ export default async function Login() {
   const t = await getTranslations();
 
   return (
-    <div className="relative flex h-full w-full flex-col items-center justify-center p-8 py-32 lg:py-48">
+    <div className="relative flex h-full w-full flex-col items-center justify-center p-8 py-16 lg:py-32">
       <div className="grid max-w-md gap-2">
         <div className="flex items-center justify-center">
           <h1 className="relative flex w-fit items-center gap-2">
