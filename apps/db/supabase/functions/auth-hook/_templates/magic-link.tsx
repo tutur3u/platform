417104e2754ea4733049fcf2e4cbd72d3f7a8644
Võ Hoàng Phúc <contact@vohoangphuc.com>
2025-03-25 16:29:52 +0700

    chore(deps): update dependencies

--- apps/db/supabase/functions/auth-hook/_templates/magic-link.tsx
+++ apps/db/supabase/functions/auth-hook/_templates/magic-link.tsx
@@ -8,7 +8,7 @@ import {
   Preview,
   Section,
   Text,
-} from 'npm:@react-email/components@0.0.22';
+} from 'npm:@react-email/components@0.0.34';
 import * as React from 'npm:react@18.3.1';
 
 interface MagicLinkEmailProps {
