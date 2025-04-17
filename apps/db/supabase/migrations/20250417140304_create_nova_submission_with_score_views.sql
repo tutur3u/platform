3955f8de76bf38d97496c71fec017005e6c55443
Nguyen Khang <nguyengiakhang646@gmail.com>
2025-04-17 18:40:46 +0700

    fix(migrations): correct column names and enhance score calculation in nova_submission views

--- apps/db/supabase/migrations/20250417140304_create_nova_submission_with_score_views.sql
+++ apps/db/supabase/migrations/20250417140304_create_nova_submission_with_score_views.sql
@@ -11,24 +11,33 @@ criteria_stats AS (
   SELECT
     submission_id,
     COUNT(*) AS total_criteria,
-    SUM(score) AS sum_criteria_score
+    SUM(score) AS sum_criterion_score
   FROM nova_submission_criteria
   GROUP BY submission_id
 )
 SELECT 
   s.*,
+
   tc.total_tests,
   tc.passed_tests,
+  (COALESCE(tc.passed_tests, 0)::float / NULLIF(tc.total_tests, 0)::float) * 5 AS test_case_score,
+
   cr.total_criteria,
-  cr.sum_criteria_score,
+  cr.sum_criterion_score,
   CASE 
     WHEN tc.total_tests > 0 THEN
-      (COALESCE(tc.passed_tests, 0)::float / NULLIF(tc.total_tests, 0)::float) * 5 + 
-      (COALESCE(cr.sum_criteria_score, 0)::float / (10 * NULLIF(cr.total_criteria, 0)::float)) * 5
+      (COALESCE(cr.sum_criterion_score, 0)::float / (10 * NULLIF(cr.total_criteria, 0)::float)) * 5
     ELSE
-      (COALESCE(cr.sum_criteria_score, 0)::float / (10 * NULLIF(cr.total_criteria, 0)::float)) * 10
-  END AS calculated_score
-FROM 
-  nova_submissions s
+      (COALESCE(cr.sum_criterion_score, 0)::float / (10 * NULLIF(cr.total_criteria, 0)::float)) * 10
+  END AS criteria_score,
+
+  ((COALESCE(tc.passed_tests, 0)::float / NULLIF(tc.total_tests, 0)::float) * 5) + 
+  (CASE 
+    WHEN tc.total_tests > 0 THEN
+      (COALESCE(cr.sum_criterion_score, 0)::float / (10 * NULLIF(cr.total_criteria, 0)::float)) * 5
+    ELSE
+      (COALESCE(cr.sum_criterion_score, 0)::float / (10 * NULLIF(cr.total_criteria, 0)::float)) * 10
+  END) AS total_score
+FROM nova_submissions s
 LEFT JOIN test_case_stats tc ON s.id = tc.submission_id
 LEFT JOIN criteria_stats cr ON s.id = cr.submission_id;
