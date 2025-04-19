5b4f115dea2a30c3a91b0a593178687d8b0476a0
Võ Hoàng Phúc <contact@vohoangphuc.com>
2025-04-19 18:31:01 +0700

    chore: migrate Prettier configuration from mjs to ts and update workflow

---
+++ prettier.config.ts
@@ -0,0 +1,18 @@
+import { type Config } from 'prettier';
+
+const config: Config = {
+  semi: true,
+  trailingComma: 'es5',
+  singleQuote: true,
+  tabWidth: 2,
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
+  plugins: [
+    '@trivago/prettier-plugin-sort-imports',
+    'prettier-plugin-tailwindcss',
+  ],
+  tailwindStylesheet: './packages/ui/src/globals.css',
+  tailwindFunctions: ['clsx', 'twMerge', 'cn', 'tw'],
+};
+
+export default config;
