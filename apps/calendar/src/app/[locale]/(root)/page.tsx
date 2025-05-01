75b0e2f0e3adf70a561a5304b8e62a2e0d077a9a
Võ Hoàng Phúc <contact@vohoangphuc.com>
2025-05-01 17:00:06 +0700

    fix(build): remove outdated props

--- apps/calendar/src/app/[locale]/(root)/page.tsx
+++ apps/calendar/src/app/[locale]/(root)/page.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import { DEV_MODE } from '@/constants/common';
 import { useQuery, useQueryClient } from '@tanstack/react-query';
 import { Calendar } from '@tuturuuu/ui/legacy/calendar/Calendar';
 import { useLocale, useTranslations } from 'next-intl';
@@ -11,22 +10,14 @@ export default function Home() {
   // const { date, setDate, view, setView, availableViews } = useCalendarContext();
 
   return (
-    <div className="relative flex h-screen flex-col overflow-y-auto p-4 pt-16 md:p-8 md:pb-4 md:pt-20 lg:p-16 lg:pb-4 lg:pt-20">
+    <div className="relative flex h-screen flex-col overflow-y-auto p-4 pt-16 md:p-8 md:pt-20 md:pb-4 lg:p-16 lg:pt-20 lg:pb-4">
       <Calendar
         t={t}
         locale={locale}
         useQuery={useQuery}
         useQueryClient={useQueryClient}
         enableHeader={false}
-        enableExperimentalGoogleCalendar={DEV_MODE}
         disabled
-        // externalState={{
-        //   date,
-        //   setDate,
-        //   view,
-        //   setView,
-        //   availableViews,
-        // }}
       />
     </div>
   );
