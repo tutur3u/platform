23d1f42ba7386f3cdbdc32f8d068109efcb81a26
Nhung <nhungmaitran1412@gmail.com>
2025-05-02 16:34:18 +0800

    feat(quiz): add quizzes page

---
+++ apps/upskii/src/app/[locale]/(dashboard)/education/quizzes/page.tsx
@@ -0,0 +1,95 @@
+import { getWorkspaceQuizColumns } from './columns';
+import QuizForm from './form';
+import { CustomDataTable } from '@/components/custom-data-table';
+import { createClient } from '@tuturuuu/supabase/next/server';
+import { WorkspaceQuiz } from '@tuturuuu/types/db';
+import FeatureSummary from '@tuturuuu/ui/custom/feature-summary';
+import { Separator } from '@tuturuuu/ui/separator';
+import { getTranslations } from 'next-intl/server';
+
+interface SearchParams {
+  q?: string;
+  page?: string;
+  pageSize?: string;
+  includedTags?: string | string[];
+  excludedTags?: string | string[];
+}
+
+interface Props {
+  params: Promise<{
+    wsId: string;
+  }>;
+  searchParams: Promise<SearchParams>;
+}
+
+export default async function WorkspaceQuizzesPage({
+  params,
+  searchParams,
+}: Props) {
+  const t = await getTranslations();
+  const { wsId } = await params;
+
+  const { data, count } = await getData(wsId, await searchParams);
+
+  return (
+    <>
+      <FeatureSummary
+        pluralTitle={t('ws-quizzes.plural')}
+        singularTitle={t('ws-quizzes.singular')}
+        description={t('ws-quizzes.description')}
+        createTitle={t('ws-quizzes.create')}
+        createDescription={t('ws-quizzes.create_description')}
+        form={<QuizForm wsId={wsId} />}
+      />
+      <Separator className="my-4" />
+      <CustomDataTable
+        data={data}
+        columnGenerator={getWorkspaceQuizColumns}
+        namespace="quiz-data-table"
+        count={count}
+        defaultVisibility={{
+          id: false,
+          created_at: false,
+        }}
+      />
+    </>
+  );
+}
+
+async function getData(
+  wsId: string,
+  {
+    q,
+    page = '1',
+    pageSize = '10',
+    retry = true,
+  }: { q?: string; page?: string; pageSize?: string; retry?: boolean } = {}
+) {
+  const supabase = await createClient();
+
+  const queryBuilder = supabase
+    .from('workspace_quizzes')
+    .select('*', {
+      count: 'exact',
+    })
+    .eq('ws_id', wsId)
+    .order('created_at', { ascending: false });
+
+  if (q) queryBuilder.ilike('name', `%${q}%`);
+
+  if (page && pageSize) {
+    const parsedPage = parseInt(page);
+    const parsedSize = parseInt(pageSize);
+    const start = (parsedPage - 1) * parsedSize;
+    const end = parsedPage * parsedSize;
+    queryBuilder.range(start, end).limit(parsedSize);
+  }
+
+  const { data, error, count } = await queryBuilder;
+  if (error) {
+    if (!retry) throw error;
+    return getData(wsId, { q, pageSize, retry: false });
+  }
+
+  return { data, count } as { data: WorkspaceQuiz[]; count: number };
+}
