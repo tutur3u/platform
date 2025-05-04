77e73a41ecd25b8747bf35ad5f52a174afe324d6
Tran Phan Trong Phuc <117657788+Henry7482@users.noreply.github.com>
2025-05-04 20:13:45 +0800

    Feat: backend api for course management
    Added the workspace api set from Tuturuu api/V1/workspace

---
+++ apps/upskii/src/app/api/v1/workspaces/[wsId]/courses/[courseId]/route.ts
@@ -0,0 +1,49 @@
+import { createClient } from '@tuturuuu/supabase/next/server';
+import { NextResponse } from 'next/server';
+
+interface Params {
+  params: Promise<{
+    courseId: string;
+  }>;
+}
+
+export async function PUT(req: Request, { params }: Params) {
+  const supabase = await createClient();
+  const data = await req.json();
+  const { courseId: id } = await params;
+
+  const { error } = await supabase
+    .from('workspace_courses')
+    .update(data)
+    .eq('id', id);
+
+  if (error) {
+    console.log(error);
+    return NextResponse.json(
+      { message: 'Error updating workspace course' },
+      { status: 500 }
+    );
+  }
+
+  return NextResponse.json({ message: 'success' });
+}
+
+export async function DELETE(_: Request, { params }: Params) {
+  const supabase = await createClient();
+  const { courseId: id } = await params;
+
+  const { error } = await supabase
+    .from('workspace_courses')
+    .delete()
+    .eq('id', id);
+
+  if (error) {
+    console.log(error);
+    return NextResponse.json(
+      { message: 'Error deleting workspace course' },
+      { status: 500 }
+    );
+  }
+
+  return NextResponse.json({ message: 'success' });
+}
