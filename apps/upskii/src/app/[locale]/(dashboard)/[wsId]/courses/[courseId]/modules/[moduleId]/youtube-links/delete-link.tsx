8586480dd0d6d059a9fa651d2d78e288777172c3
Tran Phan Trong Phuc <117657788+Henry7482@users.noreply.github.com>
2025-05-06 01:17:13 +0700

    feat(courses): workspace layout and dependencies
    Add UI for course and module details
    Add workspace dependencies for course management
    
    Workspace still must be hardcoded to be able to run the pages
    File upload has an unknown error

---
+++ apps/upskii/src/app/[locale]/(dashboard)/[wsId]/courses/[courseId]/modules/[moduleId]/youtube-links/delete-link.tsx
@@ -0,0 +1,65 @@
+'use client';
+
+import { createClient } from '@tuturuuu/supabase/next/client';
+import { Button } from '@tuturuuu/ui/button';
+import { Trash } from '@tuturuuu/ui/icons';
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
+
+export default function DeleteLinkButton({
+  moduleId,
+  courseId,
+  link,
+  links,
+}: {
+  moduleId: string;
+  courseId: string;
+  link: string;
+  links: string[];
+}) {
+  const router = useRouter();
+  const [loading, setLoading] = useState(false);
+
+  const updateYoutubeLinks = async (
+    moduleId: string,
+    courseId: string,
+    links: string[]
+  ) => {
+    setLoading(true);
+    const supabase = createClient();
+
+    const { data, error } = await supabase
+      .from('workspace_course_modules')
+      .update({
+        youtube_links: links,
+      })
+      .eq('id', moduleId)
+      .eq('course_id', courseId);
+
+    if (error) {
+      console.error('error', error);
+      setLoading(false);
+    } else {
+      router.refresh();
+    }
+
+    return data;
+  };
+
+  return (
+    <Button
+      onClick={async () => {
+        await updateYoutubeLinks(
+          moduleId,
+          courseId,
+          links.filter((l) => l !== link)
+        );
+      }}
+      size="icon"
+      variant="destructive"
+      disabled={loading}
+    >
+      <Trash className="h-5 w-5" />
+    </Button>
+  );
+}
---
+++ apps/upskii/src/app/[locale]/(dashboard)/[wsId]/drive/table.tsx
@@ -0,0 +1,62 @@
+'use client';
+
+import { storageObjectsColumns } from './columns';
+import { CustomDataTable } from '@/components/custom-data-table';
+import { StorageObject } from '@tuturuuu/types/primitives/StorageObject';
+import { Dialog } from '@tuturuuu/ui/dialog';
+import { useState } from 'react';
+
+interface Props {
+  wsId: string;
+  data: StorageObject[];
+  path?: string;
+  count: number;
+}
+
+export default function StorageObjectsTable({
+  wsId,
+  data,
+  path,
+  count,
+}: Props) {
+  // const t = useTranslations('common');
+
+  const [storageObj, setStorageObject] = useState<StorageObject>();
+
+  // const onComplete = () => {
+  //   setStorageObject(undefined);
+  // };
+
+  return (
+    <Dialog
+      open={!!storageObj}
+      onOpenChange={(open) =>
+        setStorageObject(open ? storageObj || {} : undefined)
+      }
+    >
+      <CustomDataTable
+        data={!path || path === '/' ? data : [{ name: '...' }, ...data]}
+        columnGenerator={(t: any, namespace: string | undefined) =>
+          storageObjectsColumns(t, namespace, setStorageObject, wsId, path)
+        }
+        namespace="storage-object-data-table"
+        count={count}
+        defaultVisibility={{
+          id: false,
+        }}
+        // newObjectTitle={t('upload')}
+        // editContent={
+        //   <>
+        //     <DialogTitle hidden />
+        //     <StorageObjectForm
+        //       wsId={wsId}
+        //       onComplete={onComplete}
+        //       uploadPath={path}
+        //       submitLabel={storageObj?.id ? t('edit') : t('upload')}
+        //     />
+        //   </>
+        // }
+      />
+    </Dialog>
+  );
+}
