754448938e7e26dfd2280d618bed2f3088017fe3
Nhung <nhungmaitran1412@gmail.com>
2025-05-10 01:05:01 +0700

    feat(upskii)(workspace): add backend for workspace invitations and other settings

---
+++ apps/upskii/src/app/api/workspaces/[wsId]/products/categories/migrate/route.ts
@@ -0,0 +1,35 @@
+import { createClient } from '@tuturuuu/supabase/next/server';
+import { ProductCategory } from '@tuturuuu/types/primitives/ProductCategory';
+import { NextResponse } from 'next/server';
+
+interface Params {
+  params: Promise<{
+    wsId: string;
+  }>;
+}
+
+export async function PUT(req: Request, { params }: Params) {
+  const supabase = await createClient();
+  const data = await req.json();
+  const { wsId } = await params;
+
+  const { error } = await supabase
+    .from('product_categories')
+    .upsert(
+      (data?.categories || []).map((c: ProductCategory) => ({
+        ...c,
+        ws_id: wsId,
+      }))
+    )
+    .eq('id', data.id);
+
+  if (error) {
+    console.log(error);
+    return NextResponse.json(
+      { message: 'Error migrating product categories' },
+      { status: 500 }
+    );
+  }
+
+  return NextResponse.json({ message: 'success' });
+}
