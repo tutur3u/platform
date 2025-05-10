cf22a8f5e731835b40eaea36e917ea71a584d781
Tran Phan Trong Phuc <117657788+Henry7482@users.noreply.github.com>
2025-05-10 22:17:22 +0700

    fix: courses bugs
    Add 'description' column for courses in supabase
    Add a div for Course Description
    Copy api for Flashcard Generation
    Set dynamic sizing for Module Video preview
    Add delete confirmation for module

--- apps/upskii/src/app/[locale]/(dashboard)/[wsId]/courses/[courseId]/modules/[moduleId]/youtube-links/embed.tsx
+++ apps/upskii/src/app/[locale]/(dashboard)/[wsId]/courses/[courseId]/modules/[moduleId]/youtube-links/embed.tsx
@@ -5,13 +5,11 @@ export const YoutubeEmbed = ({ embedId }: { embedId: string | undefined }) => {
 
   return embedId ? (
     <iframe
-      width="853"
-      height="480"
       src={`https://www.youtube.com/embed/${embedId}`}
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
       allowFullScreen
       title="Embedded youtube"
-      className="aspect-video w-full h-64 md:h-96 lg:h-[600px] rounded-lg"
+      className="w-full h-full rounded-lg"
     />
   ) : (
     <div>{t('ws-course-modules.invalid_youtube_link')}.</div>
