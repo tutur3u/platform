c0a6d7c116bdcc6c45682c210708582a8e8cd9a9
VNOsST <118870645+VNOsST@users.noreply.github.com>
2025-05-12 15:36:27 +0000

    style: apply prettier formatting

--- apps/upskii/src/app/[locale]/(dashboard)/onboarding/page.tsx
+++ apps/upskii/src/app/[locale]/(dashboard)/onboarding/page.tsx
@@ -2,14 +2,14 @@ import WorkspaceInvites from './workspace-invites';
 import { LoadingIndicator } from '@tuturuuu/ui/custom/loading-indicator';
 import { Separator } from '@tuturuuu/ui/separator';
 import { getTranslations } from 'next-intl/server';
-import { Suspense } from 'react';
 import Link from 'next/link';
+import { Suspense } from 'react';
 
 export default async function OnboardingPage() {
   const t = await getTranslations('onboarding');
 
   return (
-    <div className="inset-0 flex h-screen items-center justify-center flex-col overflow-scroll p-4 lg:px-32">
+    <div className="inset-0 flex h-screen flex-col items-center justify-center overflow-scroll p-4 lg:px-32">
       <div className="border-foreground/20 flex max-h-full w-full max-w-2xl flex-col items-center gap-4 rounded-xl border p-4 backdrop-blur-2xl md:p-8">
         <Suspense
           fallback={
