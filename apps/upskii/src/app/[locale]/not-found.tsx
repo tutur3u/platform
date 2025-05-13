85df333c54d8c88a4dfcef4b2fffd56fe9265e57
Tôn Thất Hữu Luân <s3958304@rmit.edu.vn>
2025-05-13 19:54:17 +0700

    feat: not found page
    Not found page implemented using fix from: https://www.reddit.com/r/nextjs/comments/144mwxi/comment/kpcfb24

--- apps/upskii/src/app/[locale]/not-found.tsx
+++ apps/upskii/src/app/[locale]/not-found.tsx
@@ -19,7 +19,7 @@ export default function NotFound() {
       </p>
 
       <Link
-        href="/onboarding"
+        href="/"
         className="mt-4 block w-fit rounded bg-blue-500/10 px-8 py-2 font-semibold text-blue-500 transition duration-300 hover:bg-blue-500/20 dark:bg-blue-300/20 dark:text-blue-300 dark:hover:bg-blue-300/30"
       >
         {backToHome}
