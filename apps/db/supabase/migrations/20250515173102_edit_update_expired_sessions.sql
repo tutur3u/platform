ba369b1e94554a087975e6843fcb3f1f3ff3df81
Nguyen Khang <nguyengiakhang646@gmail.com>
2025-05-16 01:05:46 +0700

    fix(db): Fix expiration time calculation for update_expired_sessions()

--- apps/db/supabase/migrations/20250515173102_edit_update_expired_sessions.sql
+++ apps/db/supabase/migrations/20250515173102_edit_update_expired_sessions.sql
@@ -3,20 +3,25 @@ CREATE OR REPLACE FUNCTION public.update_expired_sessions()
  LANGUAGE plpgsql
 AS $function$
 BEGIN
+  WITH expiration_time AS (
+    SELECT 
+      ns.id,
+      CASE 
+        WHEN nc.close_at IS NULL THEN ns.start_time + (nc.duration * interval '1 second')
+        ELSE LEAST(ns.start_time + (nc.duration * interval '1 second'), nc.close_at)
+      END AS exp_time
+    FROM nova_sessions ns
+    JOIN nova_challenges nc ON ns.challenge_id = nc.id
+    WHERE ns.end_time IS NULL
+    AND ns.status != 'ENDED'
+  )
   UPDATE nova_sessions ns
   SET
-    end_time = (
-      SELECT 
-        CASE 
-          WHEN nc.close_at IS NULL THEN ns.start_time + (nc.duration * interval '1 second')
-          ELSE LEAST(ns.start_time + (nc.duration * interval '1 second'), nc.close_at)
-        END
-    ),
+    end_time = et.exp_time,
     status = 'ENDED'
-  FROM nova_challenges nc
-  WHERE ns.challenge_id = nc.id
-  AND ns.start_time + (nc.duration * interval '1 second') <= now()
-  AND ns.status != 'ENDED';
+  FROM expiration_time et
+  WHERE ns.id = et.id
+  AND et.exp_time <= now();
 END;
 $function$
 ;
