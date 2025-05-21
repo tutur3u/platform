ce36f175ff1101fc1a280d195e1a4ab75a5d4e22
VNOsST <118870645+VNOsST@users.noreply.github.com>
2025-05-21 16:07:45 +0000

    style: apply prettier formatting

--- apps/upskii/src/components/text-editor/tool-bar.tsx
+++ apps/upskii/src/components/text-editor/tool-bar.tsx
@@ -1,19 +1,19 @@
-import { Editor } from "@tiptap/react";
+import { Editor } from '@tiptap/react';
 import {
-    AlignCenter,
-    AlignLeft,
-    AlignRight,
-    Bold,
-    Heading1,
-    Heading2,
-    Heading3,
-    Highlighter,
-    Italic,
-    List,
-    ListOrdered,
-    Strikethrough,
-} from "@tuturuuu/ui/icons";
-import { Toggle } from "@tuturuuu/ui/toggle";
+  AlignCenter,
+  AlignLeft,
+  AlignRight,
+  Bold,
+  Heading1,
+  Heading2,
+  Heading3,
+  Highlighter,
+  Italic,
+  List,
+  ListOrdered,
+  Strikethrough,
+} from '@tuturuuu/ui/icons';
+import { Toggle } from '@tuturuuu/ui/toggle';
 
 export default function ToolBar({ editor }: { editor: Editor | null }) {
   if (!editor) {
@@ -24,67 +24,67 @@ export default function ToolBar({ editor }: { editor: Editor | null }) {
     {
       icon: <Heading1 className="size-4" />,
       onClick: () => editor.chain().focus().toggleHeading({ level: 1 }).run(),
-      preesed: editor.isActive("heading", { level: 1 }),
+      preesed: editor.isActive('heading', { level: 1 }),
     },
     {
       icon: <Heading2 className="size-4" />,
       onClick: () => editor.chain().focus().toggleHeading({ level: 2 }).run(),
-      preesed: editor.isActive("heading", { level: 2 }),
+      preesed: editor.isActive('heading', { level: 2 }),
     },
     {
       icon: <Heading3 className="size-4" />,
       onClick: () => editor.chain().focus().toggleHeading({ level: 3 }).run(),
-      preesed: editor.isActive("heading", { level: 3 }),
+      preesed: editor.isActive('heading', { level: 3 }),
     },
     {
       icon: <Bold className="size-4" />,
       onClick: () => editor.chain().focus().toggleBold().run(),
-      preesed: editor.isActive("bold"),
+      preesed: editor.isActive('bold'),
     },
     {
       icon: <Italic className="size-4" />,
       onClick: () => editor.chain().focus().toggleItalic().run(),
-      preesed: editor.isActive("italic"),
+      preesed: editor.isActive('italic'),
     },
     {
       icon: <Strikethrough className="size-4" />,
       onClick: () => editor.chain().focus().toggleStrike().run(),
-      preesed: editor.isActive("strike"),
+      preesed: editor.isActive('strike'),
     },
     {
       icon: <AlignLeft className="size-4" />,
-      onClick: () => editor.chain().focus().setTextAlign("left").run(),
-      preesed: editor.isActive({ textAlign: "left" }),
+      onClick: () => editor.chain().focus().setTextAlign('left').run(),
+      preesed: editor.isActive({ textAlign: 'left' }),
     },
     {
       icon: <AlignCenter className="size-4" />,
-      onClick: () => editor.chain().focus().setTextAlign("center").run(),
-      preesed: editor.isActive({ textAlign: "center" }),
+      onClick: () => editor.chain().focus().setTextAlign('center').run(),
+      preesed: editor.isActive({ textAlign: 'center' }),
     },
     {
       icon: <AlignRight className="size-4" />,
-      onClick: () => editor.chain().focus().setTextAlign("right").run(),
-      preesed: editor.isActive({ textAlign: "right" }),
+      onClick: () => editor.chain().focus().setTextAlign('right').run(),
+      preesed: editor.isActive({ textAlign: 'right' }),
     },
     {
       icon: <List className="size-4" />,
       onClick: () => editor.chain().focus().toggleBulletList().run(),
-      preesed: editor.isActive("bulletList"),
+      preesed: editor.isActive('bulletList'),
     },
     {
       icon: <ListOrdered className="size-4" />,
       onClick: () => editor.chain().focus().toggleOrderedList().run(),
-      preesed: editor.isActive("orderedList"),
+      preesed: editor.isActive('orderedList'),
     },
     {
       icon: <Highlighter className="size-4" />,
       onClick: () => editor.chain().focus().toggleHighlight().run(),
-      preesed: editor.isActive("highlight"),
+      preesed: editor.isActive('highlight'),
     },
   ];
 
   return (
-    <div className="border rounded-md p-1 mb-1 bg-foreground/5 border-slate-200 dark:border-gray-950 space-x-2 z-50">
+    <div className="bg-foreground/5 z-50 mb-1 space-x-2 rounded-md border border-slate-200 p-1 dark:border-gray-950">
       {Options.map((option, index) => (
         <Toggle
           key={index}
