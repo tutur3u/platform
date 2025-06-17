aa5c52ac2bb0182f137fd4bd615bb0671bbddcc7
Võ Hoàng Phúc <contact@vohoangphuc.com>
2025-06-17 23:52:49 +0700

    chore(db): consolidate database schema

--- apps/db/supabase/migrations/20250616135112_add_platform_services.sql
+++ apps/db/supabase/migrations/20250616135112_add_platform_services.sql
@@ -2,7 +2,9 @@ create type "public"."platform_service" as enum ('TUTURUUU', 'REWISE', 'NOVA', '
 
 alter table "public"."users" add column "services" platform_service[];
 
-alter table "public"."users" alter column "services" set default '{TUTURUUU}';
+ALTER TABLE public.users
+  ALTER COLUMN services SET DEFAULT '{TUTURUUU}',
+  ALTER COLUMN services SET NOT NULL;
 
 -- Update existing users to have the default service if they don't have any services
 UPDATE public.users 
