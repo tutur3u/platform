6b3641182825feb6890e8337936912e4b2da0128
Võ Hoàng Phúc <contact@vohoangphuc.com>
2025-07-04 00:03:49 +0700

    chore!: remove upskii project

--- apps/upskii/src/utils/text-helper.ts
+++
@@ -1,8 +0,0 @@
-export const removeAccents = (str: string) =>
-  str
-    .normalize('NFD')
-    .replace(/[\u0300-\u036f]/g, '')
-    // specifically replace "đ" with "d" (both lowercase and uppercase)
-    // to support Vietnamese characters
-    .replace(/đ/g, 'd')
-    .replace(/Đ/g, 'D');
