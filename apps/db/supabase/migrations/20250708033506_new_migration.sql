2a995e30c57427edc94068420c9bf61f975fd2c8
phatgg221 <129394719+phatgg221@users.noreply.github.com>
2025-07-08 10:35:57 +0700

    fix: type inconsistent in enumerate types

---
+++ apps/db/supabase/migrations/20250708033506_new_migration.sql
@@ -0,0 +1,15 @@
+alter table "public"."tasks" alter column "user_defined_priority" drop default;
+
+alter type "public"."task_priority" rename to "task_priority__old_version_to_be_dropped";
+
+create type "public"."task_priority" as enum ('low', 'normal', 'high', 'critical');
+
+alter table "public"."tasks" alter column user_defined_priority type "public"."task_priority" using user_defined_priority::text::"public"."task_priority";
+
+alter table "public"."tasks" alter column "user_defined_priority" set default 'medium'::task_priority;
+
+drop type "public"."task_priority__old_version_to_be_dropped";
+
+alter table "public"."tasks" alter column "user_defined_priority" set default 'normal'::task_priority;
+
+
