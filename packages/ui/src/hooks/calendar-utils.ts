be9e3666f82e8a5b3e2bcb579950a9b25d577bbc
DennieDan <e1129903@u.nus.edu>
2025-07-14 23:41:35 +0800

    fix: Update packages/ui/src/hooks/calendar-utils.ts

--- packages/ui/src/hooks/calendar-utils.ts
+++ packages/ui/src/hooks/calendar-utils.ts
@@ -8,8 +8,8 @@ export function isAllDayEvent(event: Pick<CalendarEvent, 'start_at' | 'end_at'>,
   const start = dayjs(event.start_at);
   const end = dayjs(event.end_at);
   
-  const isStartAtMidnight = start.hour() === 0 && start.minute() === 0 && start.second() === 0;
-  const isEndAtMidnight = end.hour() === 0 && end.minute() === 0 && end.second() === 0;
+  const isStartAtMidnight = start.hour() === 0 && start.minute() === 0 && start.second() === 0 && start.millisecond() === 0;
+  const isEndAtMidnight = end.hour() === 0 && end.minute() === 0 && end.second() === 0 && end.millisecond() === 0;
   const durationHours = end.diff(start, 'hour');
   const isMultipleOf24Hours = durationHours > 0 && durationHours % 24 === 0;
   
