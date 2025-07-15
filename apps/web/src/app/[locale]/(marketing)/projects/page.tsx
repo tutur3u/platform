3e94fa266f0f30e5f62b6e9284fbd6921eb82cf9
Nguyen Khang <nguyengiakhang646@gmail.com>
2025-07-15 22:47:34 +0700

    feat(projects): Move flagship project to a separate component

--- apps/web/src/app/[locale]/(marketing)/projects/page.tsx
+++ apps/web/src/app/[locale]/(marketing)/projects/page.tsx
@@ -1,11 +1,11 @@
+import MainProject from './main-project';
 import Projects from './projects';
 
 export default function ProjectsPage() {
   return (
-    <div className="flex justify-center">
-      <div className="container flex flex-col items-center gap-6 text-foreground">
-        <Projects />
-      </div>
+    <div className="container flex flex-col items-center gap-6 text-foreground">
+      <MainProject />
+      <Projects />
     </div>
   );
 }
