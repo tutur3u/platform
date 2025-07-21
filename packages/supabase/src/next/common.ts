87792071952a6bc1c2113eefa3c7cbb7577b8f16
phatgg221 <129394719+phatgg221@users.noreply.github.com>
2025-07-21 14:48:05 +0700

    fix!: return to static variable

--- packages/supabase/src/next/common.ts
+++ packages/supabase/src/next/common.ts
@@ -15,9 +15,12 @@ export function checkEnvVariables({
   key: string;
 } {
   // eslint-disable-next-line no-undef
-  const url = 'http://127.0.0.1:8001';
-  const key =
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImV4cCI6MTk4MzgxMjk5Nn0.EGIM96RAZx35lJzdJsyH-qQwv8Hdp7fsn3W0YpN81IU';
+  const url = process.env.NEXT_PUBLIC_SUPABASE_URL;
+  const key = useServiceKey
+    ? // eslint-disable-next-line no-undef
+      process.env.SUPABASE_SERVICE_KEY || process.env.SUPABASE_SERVICE_ROLE_KEY
+    : // eslint-disable-next-line no-undef
+      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
   if (!url) throw Error('Missing Supabase URL');
   if (!key) throw Error(`Missing Supabase key`);
