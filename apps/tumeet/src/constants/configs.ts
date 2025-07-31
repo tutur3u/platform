36a1e0f721e7185c4dbac666746d32244aa68d8e
Võ Hoàng Phúc <contact@vohoangphuc.com>
2025-07-31 14:56:03 +0700

    chore: update site configuration for Tumeet
    - Changed site name from 'Rewise' to 'Tumeet'.
    - Updated development and production URLs to reflect the new site name and port.

--- apps/tumeet/src/constants/configs.ts
+++ apps/tumeet/src/constants/configs.ts
@@ -1,11 +1,11 @@
 import { DEV_MODE } from './common';
 
 export const siteConfig = {
-  name: 'Rewise',
-  url: DEV_MODE ? 'http://localhost:7804' : 'https://rewise.me',
+  name: 'Tumeet',
+  url: DEV_MODE ? 'http://localhost:7807' : 'https://tumeet.me',
   ogImage: DEV_MODE
-    ? 'http://localhost:7804/media/logos/og-image.png'
-    : 'https://rewise.me/media/logos/og-image.png',
+    ? 'http://localhost:7807/media/logos/og-image.png'
+    : 'https://tumeet.me/media/logos/og-image.png',
   links: {
     twitter: 'https://twitter.com/tutur3u',
     github: 'https://github.com/tutur3u/platform',
