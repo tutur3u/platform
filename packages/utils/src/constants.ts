29defd4dc7df712526eb93ed64f64148bd849cf0
DennieDan <e1129903@u.nus.edu>
2025-08-01 22:25:35 +0800

    feat: define DEV_MODE constant

--- packages/utils/src/constants.ts
+++ packages/utils/src/constants.ts
@@ -1,3 +1,6 @@
 export const GITHUB_OWNER = 'tutur3u';
 export const GITHUB_REPO = 'platform';
 export const ROOT_WORKSPACE_ID = '00000000-0000-0000-0000-000000000000';
+
+export const DEV_MODE = process.env.NODE_ENV === 'development';
+export const PROD_MODE = process.env.NODE_ENV === 'production';
