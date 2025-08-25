deeda0f73c02674341fa957df36c6652551c7d58
DennieDan <e1129903@u.nus.edu>
2025-08-25 09:21:39 +0800

    fix: declare Request arg and define Params type

--- apps/web/src/app/api/workspaces/[wsId]/is-personal/route.ts
+++ apps/web/src/app/api/workspaces/[wsId]/is-personal/route.ts
@@ -1,10 +1,17 @@
 import { isPersonalWorkspace } from '@tuturuuu/utils/workspace-helper';
 import { NextResponse } from 'next/server';
 
+interface Params {
+    params: Promise<{
+      wsId: string;
+    }>;
+  }
+
 export async function GET(
-  { params }: { params: { wsId: string } }
+  _: Request,
+  { params }: Params
 ) {
-  const { wsId } = params;
-  const isPersonal = isPersonalWorkspace(wsId);
+  const { wsId } = await params;
+  const isPersonal = await isPersonalWorkspace(wsId);
   return NextResponse.json({ isPersonal: isPersonal });
 }
