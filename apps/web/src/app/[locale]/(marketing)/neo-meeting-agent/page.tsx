5a57ddd31de1a9e49d6cdb0af5ec03497433909e
koromaki88 <officialngannhien@gmail.com>
2025-08-26 23:30:26 +0700

    feat(web): en&vi headline text for Meeting Agent page

--- apps/web/src/app/[locale]/(marketing)/neo-meeting-agent/page.tsx
+++ apps/web/src/app/[locale]/(marketing)/neo-meeting-agent/page.tsx
@@ -1,5 +1,21 @@
-'use client';
+import GradientHeadline from '@/app/[locale]/(marketing)/gradient-headline';
+import { Separator } from '@ncthub/ui/separator';
+import { getTranslations } from 'next-intl/server';
 
-export default function NeoMeetingAgentPage() {
-  return <div className=""></div>;
+export default async function NeoMeetingAgentPage() {
+  const t = await getTranslations('neo-meeting-agent');
+
+  return (
+    <div className="flex w-full flex-col items-center">
+      <div className="container mx-auto mt-8 flex max-w-6xl flex-col gap-6 px-3 py-16 lg:gap-14 lg:py-24">
+        <div className="flex flex-col items-center">
+          <h1 className="mx-auto mb-2 text-center text-2xl leading-tight! font-bold tracking-tight text-balance text-foreground md:text-4xl lg:text-6xl">
+            {t('headline-p1')}{' '}
+            <GradientHeadline>{t('headline-p2')}</GradientHeadline>.
+          </h1>
+        </div>
+      </div>
+      <Separator className="mt-8 mb-4 md:mt-16" />
+    </div>
+  );
 }
