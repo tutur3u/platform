ae3e44abd43d9bd5709827b2bbd8f58788f4fd8c
Nguyen Khang <nguyengiakhang646@gmail.com>
2025-09-02 18:29:13 +0700

    Revert "refactor(supabase): simplify client creation functions and update type exports"
    This reverts commit ac5452b400bf5f48fd7cd54237e2de6e70d77b04.

--- packages/supabase/src/next/client.ts
+++ packages/supabase/src/next/client.ts
@@ -1,18 +1,20 @@
 import { checkEnvVariables } from './common';
-import type { Database } from '@ncthub/types/supabase';
+import { Database } from '@ncthub/types/supabase';
 import { createBrowserClient } from '@supabase/ssr';
+import type { SupabaseClient } from '@supabase/supabase-js';
 
 const { url, key } = checkEnvVariables({ useServiceKey: false });
 
-export function createDynamicClient() {
+export function createDynamicClient(): SupabaseClient<any, 'public', any> {
   return createBrowserClient(url, key);
 }
 
-export function createClient() {
+export function createClient(): SupabaseClient<
+  Database,
+  'public',
+  Database['public']
+> {
   return createBrowserClient<Database>(url, key);
 }
 
-type TypedSupabaseClient = ReturnType<typeof createClient>;
-type SupabaseClient = TypedSupabaseClient;
-
-export type { TypedSupabaseClient, SupabaseClient };
+export type { SupabaseClient };
