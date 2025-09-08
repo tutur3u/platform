e1dd65cef774328b7c681907666344d0c112d96f
Võ Hoàng Phúc <contact@vohoangphuc.com>
2025-09-08 14:10:49 +0700

    refactor: remove unused locale dependency in NavbarCalendarWrapper for cleaner code

--- apps/calendar/src/app/[locale]/(root)/navbar-calendar-wrapper.tsx
+++ apps/calendar/src/app/[locale]/(root)/navbar-calendar-wrapper.tsx
@@ -1,9 +1,7 @@
 'use client';
 
 import NavbarCalendarHeader from './navbar-calendar-header';
-import { useLocale } from 'next-intl';
 
 export default function NavbarCalendarWrapper() {
-  const locale = useLocale();
-  return <NavbarCalendarHeader locale={locale} />;
+  return <NavbarCalendarHeader />;
 }
