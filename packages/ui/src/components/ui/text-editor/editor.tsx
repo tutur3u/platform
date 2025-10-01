d8c36830731af1e4f345793470fd7318610de60d
Võ Hoàng Phúc <contact@vohoangphuc.com>
2025-10-01 19:56:55 +0700

    feat(ui): implement video node support in text editor and update dependencies

--- packages/ui/src/components/ui/text-editor/editor.tsx
+++ packages/ui/src/components/ui/text-editor/editor.tsx
@@ -8,15 +8,157 @@ import Subscript from '@tiptap/extension-subscript';
 import Superscript from '@tiptap/extension-superscript';
 import TextAlign from '@tiptap/extension-text-align';
 import Youtube from '@tiptap/extension-youtube';
-import { EditorContent, type JSONContent, useEditor } from '@tiptap/react';
+import {
+  EditorContent,
+  type JSONContent,
+  Node,
+  nodeInputRule,
+  useEditor,
+} from '@tiptap/react';
 import StarterKit from '@tiptap/starter-kit';
 import { toast } from '@tuturuuu/ui/sonner';
 import { debounce } from 'lodash';
+import { Plugin, PluginKey } from 'prosemirror-state';
 import { useCallback, useEffect, useMemo, useRef, useState } from 'react';
 import ImageResize from 'tiptap-extension-resize-image';
-// @expect-error No types available
-import Video from 'tiptap-extension-video';
 import { ToolBar } from './tool-bar';
+
+declare module '@tiptap/core' {
+  interface Commands<ReturnType> {
+    video: {
+      /**
+       * Set a video node
+       */
+      setVideo: (src: string) => ReturnType;
+      /**
+       * Toggle a video
+       */
+      toggleVideo: (src: string) => ReturnType;
+    };
+  }
+}
+
+const VIDEO_INPUT_REGEX = /!\[(.+|:?)]\((\S+)(?:(?:\s+)["'](\S+)["'])?\)/;
+
+const Video = Node.create({
+  name: 'video',
+
+  group: 'block',
+
+  addAttributes() {
+    return {
+      src: {
+        default: null,
+        parseHTML: (el) => (el as HTMLSpanElement).getAttribute('src'),
+        renderHTML: (attrs) => ({ src: attrs.src }),
+      },
+    };
+  },
+
+  parseHTML() {
+    return [
+      {
+        tag: 'video',
+        getAttrs: (el) => ({
+          src: (el as HTMLVideoElement).getAttribute('src'),
+        }),
+      },
+    ];
+  },
+
+  renderHTML({ HTMLAttributes }) {
+    return [
+      'video',
+      { controls: 'true', style: 'width: 100%', ...HTMLAttributes },
+      ['source', HTMLAttributes],
+    ];
+  },
+
+  addCommands() {
+    return {
+      setVideo:
+        (src: string) =>
+        ({ commands }) =>
+          commands.insertContent(
+            `<video controls="true" style="width: 100%" src="${src}" />`
+          ),
+
+      toggleVideo:
+        () =>
+        ({ commands }) =>
+          commands.toggleNode(this.name, 'paragraph'),
+    };
+  },
+
+  addInputRules() {
+    return [
+      nodeInputRule({
+        find: VIDEO_INPUT_REGEX,
+        type: this.type,
+        getAttributes: (match) => {
+          const [, , src] = match;
+
+          return { src };
+        },
+      }),
+    ];
+  },
+
+  addProseMirrorPlugins() {
+    return [
+      new Plugin({
+        key: new PluginKey('videoDropPlugin'),
+
+        props: {
+          handleDOMEvents: {
+            drop(view, event) {
+              const {
+                state: { schema, tr },
+                dispatch,
+              } = view;
+              const hasFiles = event.dataTransfer?.files?.length;
+
+              if (!hasFiles) return false;
+
+              const videos = Array.from(event.dataTransfer.files).filter(
+                (file) => /video/i.test(file.type)
+              );
+
+              if (videos.length === 0) return false;
+
+              event.preventDefault();
+
+              const coordinates = view.posAtCoords({
+                left: event.clientX,
+                top: event.clientY,
+              });
+
+              videos.forEach((video) => {
+                const reader = new FileReader();
+
+                reader.onload = (readerEvent) => {
+                  const node = schema.nodes.video?.create({
+                    src: readerEvent.target?.result,
+                  });
+
+                  if (!node) return;
+                  if (coordinates && typeof coordinates.pos === 'number') {
+                    const transaction = tr.insert(coordinates?.pos, node);
+                    dispatch(transaction);
+                  }
+                };
+
+                reader.readAsDataURL(video);
+              });
+
+              return true;
+            },
+          },
+        },
+      }),
+    ];
+  },
+});
 
 const hasContent = (node: JSONContent): boolean => {
   // Check for text content
@@ -188,11 +330,8 @@ export function RichTextEditor({
         },
       }),
       Video.configure({
-        inline: false,
-        allowBase64: false,
         HTMLAttributes: {
           class: 'rounded-md my-4',
-          controls: true,
         },
       }),
     ],
