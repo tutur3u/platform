6cd41b207d788cfcdb465d8c3d6238392a8cfc31
Võ Hoàng Phúc <contact@vohoangphuc.com>
2025-10-16 00:31:17 +0700

    feat: add workspace invite link and usage types to database schema

--- apps/db/supabase/migrations/20230708082512_fix_auth_sign_up.sql
+++ apps/db/supabase/migrations/20230708082512_fix_auth_sign_up.sql
@@ -4,7 +4,7 @@ select case
         when rec is null then null
         when pkey_cols = array []::text [] then gen_random_uuid()
         else (
-            select uuid_generate_v5(
+            select extensions.uuid_generate_v5(
                     'fd62bc3d-8d6e-43c2-919c-802ba3762271',
                     (
                         jsonb_build_array(to_jsonb($1)) || jsonb_agg($3->>key_)
