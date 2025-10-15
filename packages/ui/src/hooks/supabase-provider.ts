0707666f437a083b9c9152720b618ee1d2fe4242
Võ Hoàng Phúc <contact@vohoangphuc.com>
2025-10-16 02:09:53 +0700

    refactor: remove unused debounce import and related event listeners

--- packages/ui/src/hooks/supabase-provider.ts
+++ packages/ui/src/hooks/supabase-provider.ts
@@ -2,7 +2,6 @@ import type { SupabaseClient } from '@tuturuuu/supabase/next/client';
 import type { RealtimeChannel } from '@tuturuuu/supabase/next/realtime';
 import debug from 'debug';
 import { EventEmitter } from 'eventemitter3';
-import { debounce } from 'lodash';
 import * as awarenessProtocol from 'y-protocols/awareness';
 import * as Y from 'yjs';
 
@@ -286,7 +285,6 @@ export default class SupabaseProvider extends EventEmitter {
     });
 
     this.connect();
-    this.doc.on('update', this.debouncedOnDocumentUpdate);
     this.awareness.on('update', this.onAwarenessUpdate.bind(this));
   }
 
@@ -369,7 +367,6 @@ export default class SupabaseProvider extends EventEmitter {
       process.off('exit', () => this.removeSelfFromAwarenessOnUnload);
     }
 
-    this.doc.off('update', this.debouncedOnDocumentUpdate);
     this.awareness.off('update', this.onAwarenessUpdate);
 
     if (this.channel) this.disconnect();
