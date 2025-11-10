db6aacf2b7260d086da2920d02b1c5aaf3ec0360
Võ Hoàng Phúc <contact@vohoangphuc.com>
2025-11-10 23:44:05 +0700

    refactor: clean up debug logging in getPermissions function

--- packages/supabase/src/next/user.ts
+++ packages/supabase/src/next/user.ts
@@ -1,13 +1,6 @@
-import type {
-  AuthenticatorAssuranceLevels,
-  JwtPayload,
-  Session,
-  User,
-} from '@supabase/supabase-js';
-
 export type {
   AuthenticatorAssuranceLevels,
   JwtPayload as SupabaseJwtPayload,
   Session as SupabaseSession,
   User as SupabaseUser,
-};
+} from '@supabase/supabase-js';
