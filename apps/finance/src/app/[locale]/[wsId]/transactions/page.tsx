128121813eccd0f67e7962c45821e46ec37854e6
Sam Ton <s3958304@rmit.edu.vn>
2026-01-22 16:12:13 +0700

    fix: build errors

--- apps/finance/src/app/[locale]/[wsId]/transactions/page.tsx
+++ apps/finance/src/app/[locale]/[wsId]/transactions/page.tsx
@@ -4,19 +4,12 @@ interface Props {
   params: Promise<{
     wsId: string;
   }>;
-  searchParams: Promise<{
-    q: string;
-    page: string;
-    pageSize: string;
-  }>;
 }
 
 export default async function WorkspaceTransactionsPage({
-  params,
-  searchParams,
+  params
 }: Props) {
   const { wsId } = await params;
-  const sp = await searchParams;
 
-  return <TransactionsPage wsId={wsId} searchParams={sp} />;
+  return <TransactionsPage wsId={wsId} />;
 }
