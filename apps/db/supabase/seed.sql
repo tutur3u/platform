8a636bcc75d28fe05b8ef37be462316276e4876f
Nguyen Khang <nguyengiakhang646@gmail.com>
2026-01-26 17:05:47 +0700

    feat: update workspace creator_id to be not null and adjust seed data accordingly

--- apps/db/supabase/seed.sql
+++ apps/db/supabase/seed.sql
@@ -307,25 +307,25 @@ values
         '00000000-0000-0000-0000-000000000001',
         'Prototype All',
         'prototype-all',
-        null
+        '00000000-0000-0000-0000-000000000001'
     ),
     (
         '00000000-0000-0000-0000-000000000002',
         'Prototype General',
         'prototype-general',
-        null
+        '00000000-0000-0000-0000-000000000001'
     ),
     (
         '00000000-0000-0000-0000-000000000003',
         'Prototype Pharmacy',
         'prototype-pharmacy',
-        null
+        '00000000-0000-0000-0000-000000000001'
     ),
     (
         '00000000-0000-0000-0000-000000000004',
         'Prototype School',
         'prototype-school',
-        null
+        '00000000-0000-0000-0000-000000000001'
     );
 
 -- Populate workspace_secrets
