e33526e973504ed214b8649b64261f0fd285944b
phuchuynhtan4103131 <s4103131@rmit.edu.vn>
2026-02-12 15:46:55 +0700

    remove redundant import line

--- apps/neo-league/src/components/navbar.tsx
+++ apps/neo-league/src/components/navbar.tsx
@@ -11,7 +11,6 @@ import {
   SheetTitle,
   SheetTrigger,
 } from '@ncthub/ui/sheet';
-import { XIcon } from 'lucide-react';
 import Image from 'next/image';
 import Link from 'next/link';
 import NavbarSeparator from './navbar-separator';
