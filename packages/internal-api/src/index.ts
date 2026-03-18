7438a2444e16d477f54e38d962c9bb22178bb9ac
Võ Hoàng Phúc <contact@vohoangphuc.com>
2026-03-18 10:21:08 +0700

    chore(deps): update dependencies

--- packages/internal-api/src/index.ts
+++ packages/internal-api/src/index.ts
@@ -1,6 +1,6 @@
 export {
-  type WorkspaceCalendarEventUpdatePayload,
   updateWorkspaceCalendarEvent,
+  type WorkspaceCalendarEventUpdatePayload,
 } from './calendar';
 export type {
   InternalApiClientOptions,
