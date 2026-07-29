085e28b9992df8aae553951b26eaf3b46f530a02
Sokora <contact@vohoangphuc.com>
2026-07-29 08:47:59 +0700

    fix(contacts): stabilize metric mutations

--- apps/contacts/src/app/[locale]/[wsId]/actions.ts
+++
@@ -1,7 +0,0 @@
-'use server';
-
-import { fetchSatelliteWorkspaces } from '@tuturuuu/satellite/workspace-actions';
-
-export async function fetchWorkspaces() {
-  return fetchSatelliteWorkspaces();
-}
